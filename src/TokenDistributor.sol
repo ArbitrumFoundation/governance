c6e1b8912b8aadeb94f997cb21247c9bd5f2e80f
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-09-26 14:53:26 +0100

    feat: token distributor

---
+++ src/TokenDistributor.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.16;
+
+import "./Util.sol";
+import "@openzeppelin/contracts-upgradeable-0.8/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable-0.8/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable-0.8/token/ERC20/IERC20Upgradeable.sol";
+
+/// @title  L2 Token Distributor
+/// @notice The L2 counterparty of the Arbitrum token.
+contract TokenDistributor is OwnableUpgradeable {
+    IERC20Upgradeable public token;
+    address public unclaimedTokensReciever;
+    mapping(address => uint256) public claimableTokens;
+    uint256 public claimPeriodStart;
+    uint256 public claimPeriodEnd;
+
+    event ClaimPeriodUpdated(uint256 start, uint256 end);
+    event Claimed(address who, uint256 amount);
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @param _token token to be distributed (assumed to be an OZ implementation)
+    function initialize(IERC20Upgradeable _token, address _unclaimedTokensReciever) external initializer {
+        token = _token;
+        unclaimedTokensReciever = _unclaimedTokensReciever;
+    }
+
+    function setRecipients(address[] calldata _recipients, uint256[] calldata _claimableAmount) external onlyOwner {
+        require(_recipients.length == _claimableAmount.length, "invalid");
+        for(uint256 i = 0; i < _recipients.length; uncheckedInc(i)) {
+            // this intentionally does not emit events to save on gas
+            claimableTokens[_recipients[i]] = _claimableAmount[i];
+        }
+        // TODO: should we check if the contract has enough tokens escrowed to cover the claimable amounts?
+    }
+
+    function setClaimPeriod(uint256 start, uint256 end) external onlyOwner {
+        require(start > block.timestamp, "invalid");
+        require(start > end, "invalid");
+        claimPeriodStart = start;
+        claimPeriodEnd = end;
+        emit ClaimPeriodUpdated(start, end);
+    }
+
+    function claim() external {
+        require(block.timestamp >= claimPeriodStart, "not started");
+        require(block.timestamp < claimPeriodEnd, "ended");
+
+        uint256 amount = claimableTokens[msg.sender];
+        require(amount > 0, "no value");
+
+        claimableTokens[msg.sender] = 0;
+        
+        // we don't use safeTransfer since impl is assumed to be OZ
+        require(token.transfer(msg.sender, amount), "fail transfer");
+        emit Claimed(msg.sender, amount);
+    }
+
+    function claimLeftovers() external {
+        require(block.timestamp >= claimPeriodEnd, "not ended");
+        uint256 leftovers = token.balanceOf(address(this));
+        require(token.transfer(unclaimedTokensReciever, leftovers), "fail transfer");
+    }
+}
---
+++ src/Util.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.16;
+
+/// @notice increments an integer without checking for overflows
+/// @dev from https://github.com/ethereum/solidity/issues/11721#issuecomment-890917517
+function uncheckedInc(uint256 x) pure returns (uint256) {
+    unchecked {
+        return x + 1;
+    }
+}
