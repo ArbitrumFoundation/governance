d322debd02d2f0ccd5787e75bb16e89ef156f5b0
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-09-27 13:33:58 +0100

    feat: claim and delegate

--- src/TokenDistributor.sol
+++ src/TokenDistributor.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.16;
 
-import "./Util.sol";
+import {uncheckedInc, IERC20VotesUpgradeable} from "./Util.sol";
+
 import "@openzeppelin/contracts-upgradeable-0.8/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable-0.8/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable-0.8/token/ERC20/IERC20Upgradeable.sol";
 
-/// @title  L2 Token Distributor
-/// @notice The L2 counterparty of the Arbitrum token.
-contract TokenDistributor is OwnableUpgradeable {
-    IERC20Upgradeable public token;
+/// @title  Token Distributor
+/// @notice A contract responsible for distributing tokens
+contract TokenDistributor is Initializable, OwnableUpgradeable {
+    IERC20VotesUpgradeable public token;
     address payable public unclaimedTokensReciever;
     mapping(address => uint256) public claimableTokens;
     uint256 public claimPeriodStart;
@@ -23,14 +23,14 @@ contract TokenDistributor is OwnableUpgradeable {
     }
 
     /// @param _token token to be distributed (assumed to be an OZ implementation)
-    function initialize(IERC20Upgradeable _token, address payable _unclaimedTokensReciever) external initializer {
+    function initialize(IERC20VotesUpgradeable _token, address payable _unclaimedTokensReciever) external initializer {
         token = _token;
         unclaimedTokensReciever = _unclaimedTokensReciever;
     }
 
     function setRecipients(address[] calldata _recipients, uint256[] calldata _claimableAmount) external onlyOwner {
         require(_recipients.length == _claimableAmount.length, "invalid");
-        for(uint256 i = 0; i < _recipients.length; uncheckedInc(i)) {
+        for (uint256 i = 0; i < _recipients.length; uncheckedInc(i)) {
             // this intentionally does not emit events to save on gas
             claimableTokens[_recipients[i]] = _claimableAmount[i];
         }
@@ -45,7 +45,29 @@ contract TokenDistributor is OwnableUpgradeable {
         emit ClaimPeriodUpdated(start, end);
     }
 
-    function claim() external {
+    /// @dev different implementations may handle validation/fail delegateBySig differently. here a OZ v4.6.0 impl is assumed
+    function claimAndDelegate(address delegatee, uint256 expiry, uint8 v, bytes32 r, bytes32 s) external {
+        claim();
+        // TODO: can we pack into tighter calldata?
+        token.delegateBySig(delegatee, 0, expiry, v, r, s);
+    }
+
+    function sweep() external {
+        require(block.timestamp >= claimPeriodEnd, "not ended");
+        uint256 leftovers = token.balanceOf(address(this));
+        require(token.transfer(unclaimedTokensReciever, leftovers), "fail transfer");
+
+        if (address(this).balance > 0) {
+            (bool success,) = unclaimedTokensReciever.call{value: address(this).balance}("");
+            require(success, "FAIL_TRANSFER");
+        }
+
+        // no funds should be sent because of previous step
+        // contract is destroyed to clean up storage
+        selfdestruct(payable(address(0)));
+    }
+
+    function claim() public {
         require(block.timestamp >= claimPeriodStart, "not started");
         require(block.timestamp < claimPeriodEnd, "ended");
 
@@ -53,16 +75,9 @@ contract TokenDistributor is OwnableUpgradeable {
         require(amount > 0, "no value");
 
         claimableTokens[msg.sender] = 0;
-        
+
         // we don't use safeTransfer since impl is assumed to be OZ
         require(token.transfer(msg.sender, amount), "fail transfer");
         emit Claimed(msg.sender, amount);
-    }
-
-    function claimLeftovers() external {
-        require(block.timestamp >= claimPeriodEnd, "not ended");
-        uint256 leftovers = token.balanceOf(address(this));
-        require(token.transfer(unclaimedTokensReciever, leftovers), "fail transfer");
-        selfdestruct(unclaimedTokensReciever);
     }
 }
--- src/Util.sol
+++ src/Util.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.16;
 
+import "@openzeppelin/contracts-upgradeable-0.8/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable-0.8/governance/utils/IVotesUpgradeable.sol";
+
 /// @notice increments an integer without checking for overflows
 /// @dev from https://github.com/ethereum/solidity/issues/11721#issuecomment-890917517
 function uncheckedInc(uint256 x) pure returns (uint256) {
@@ -8,3 +11,6 @@ function uncheckedInc(uint256 x) pure returns (uint256) {
         return x + 1;
     }
 }
+
+/// @title A token contract with governance capabilities
+interface IERC20VotesUpgradeable is IVotesUpgradeable, IERC20Upgradeable {}
