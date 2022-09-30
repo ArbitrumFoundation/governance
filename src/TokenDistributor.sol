e6bb9ddf0e676cd1279663b59a207c5b722d49bc
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-09-30 14:49:04 +0100

    respond to pr comments

--- src/TokenDistributor.sol
+++ src/TokenDistributor.sol
@@ -12,11 +12,14 @@ contract TokenDistributor is Initializable, OwnableUpgradeable {
     IERC20VotesUpgradeable public token;
     address payable public unclaimedTokensReciever;
     mapping(address => uint256) public claimableTokens;
+    uint256 public totalClaimable;
     uint256 public claimPeriodStart;
     uint256 public claimPeriodEnd;
 
     event ClaimPeriodUpdated(uint256 start, uint256 end);
     event Claimed(address who, uint256 amount);
+    event RecipientClaimableSet(address who, uint256 amount);
+    event Swept(uint256 amount);
 
     constructor() {
         _disableInitializers();
@@ -28,18 +31,29 @@ contract TokenDistributor is Initializable, OwnableUpgradeable {
         unclaimedTokensReciever = _unclaimedTokensReciever;
     }
 
+    function depositToken(uint256 amount) external {
+        require(token.transferFrom(msg.sender, address(this), amount), "TokenDistributor: fail transfer token");
+    }
+
     function setRecipients(address[] calldata _recipients, uint256[] calldata _claimableAmount) external onlyOwner {
-        require(_recipients.length == _claimableAmount.length, "invalid");
+        require(_recipients.length == _claimableAmount.length, "TokenDistributor: invalid array length");
+        uint256 sum = 0;
         for (uint256 i = 0; i < _recipients.length; uncheckedInc(i)) {
-            // this intentionally does not emit events to save on gas
+            require(claimableTokens[_recipients[i]] == 0, "TokenDistributor: recipient already set");
             claimableTokens[_recipients[i]] = _claimableAmount[i];
+            unchecked {
+                sum += _claimableAmount[i];
+            }
         }
-        // TODO: should we check if the contract has enough tokens escrowed to cover the claimable amounts?
+
+        sum += totalClaimable;
+        totalClaimable = sum;
+        require(token.balanceOf(address(this)) >= sum, "TokenDistributor: not enough balance");
     }
 
     function setClaimPeriod(uint256 start, uint256 end) external onlyOwner {
-        require(start > block.timestamp, "invalid");
-        require(start > end, "invalid");
+        require(start > block.timestamp, "TokenDistributor: start should be in the future");
+        require(end > start, "TokenDistributor: start should be before end");
         claimPeriodStart = start;
         claimPeriodEnd = end;
         emit ClaimPeriodUpdated(start, end);
@@ -53,31 +67,33 @@ contract TokenDistributor is Initializable, OwnableUpgradeable {
     }
 
     function sweep() external {
-        require(block.timestamp >= claimPeriodEnd, "not ended");
+        require(block.timestamp >= claimPeriodEnd, "TokenDistributor: not ended");
         uint256 leftovers = token.balanceOf(address(this));
-        require(token.transfer(unclaimedTokensReciever, leftovers), "fail transfer");
+        require(token.transfer(unclaimedTokensReciever, leftovers), "TokenDistributor: fail token transfer");
+
+        emit Swept(leftovers);
 
         if (address(this).balance > 0) {
+            // we transfer eth using an explicit call to make sure the receiver's fallback function is triggered
             (bool success,) = unclaimedTokensReciever.call{value: address(this).balance}("");
-            require(success, "FAIL_TRANSFER");
+            require(success, "TokenDistributor: fail eth transfer");
         }
-
         // no funds should be sent because of previous step
         // contract is destroyed to clean up storage
         selfdestruct(payable(address(0)));
     }
 
     function claim() public {
-        require(block.timestamp >= claimPeriodStart, "not started");
-        require(block.timestamp < claimPeriodEnd, "ended");
+        require(block.timestamp >= claimPeriodStart, "TokenDistributor: not started");
+        require(block.timestamp < claimPeriodEnd, "TokenDistributor: ended");
 
         uint256 amount = claimableTokens[msg.sender];
-        require(amount > 0, "no value");
+        require(amount > 0, "TokenDistributor: no value");
 
         claimableTokens[msg.sender] = 0;
 
         // we don't use safeTransfer since impl is assumed to be OZ
-        require(token.transfer(msg.sender, amount), "fail transfer");
+        require(token.transfer(msg.sender, amount), "TokenDistributor: fail token transfer");
         emit Claimed(msg.sender, amount);
     }
 }
