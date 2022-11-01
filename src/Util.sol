9030a987e57c390d9d2b9f7a8cea54b2a8f781cf
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-11-01 14:17:30 -0300

    feat: token distributor contract (#6)
    * feat: token distributor
    
    * feat: add storage cleanup after distribution
    
    * feat: claim and delegate
    
    * respond to pr comments
    
    * add pause/unpause control flow to distributor
    
    * add missing event and pause in constructor
    
    * chore: fmt
    
    * docs: add natspec comments to public interface
    
    * refactor: use block number instead of timestamp
    
    * fix: add missing inits
    
    * respond to pr comments
    
    * Update deps and add extra claimPeriodEnd zero check to sweep
    
    * Updates from code review
    
    * Update src/TokenDistributor.sol
    
    Co-authored-by: gzeon <95478735+gzeoneth@users.noreply.github.com>
    
    * Dont need to run tests on push
    
    * Merged workflows
    
    * Renamed jobs
    
    * Completed merge of workflows
    
    * remove outdated "when paused" comment
    
    Co-authored-by: Chris Buckland <cpbuckland88@gmail.com>
    Co-authored-by: gzeon <95478735+gzeoneth@users.noreply.github.com>
    Co-authored-by: dzgoldman <dzgoldman@wesleyan.edu>

---
+++ src/Util.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
+
+/// @notice increments an integer without checking for overflows
+/// @dev from https://github.com/ethereum/solidity/issues/11721#issuecomment-890917517
+function uncheckedInc(uint256 x) pure returns (uint256) {
+    unchecked {
+        return x + 1;
+    }
+}
+
+/// @title A token contract with governance capabilities
+interface IERC20VotesUpgradeable is IVotesUpgradeable, IERC20Upgradeable {}
