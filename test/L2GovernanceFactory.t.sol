d401e02239b83b19d9e75a474b7d9dad798b0334
dzgoldman <dzgoldman@wesleyan.edu>
2022-11-10 09:07:46 -0500

    more tests

--- test/L2GovernanceFactory.t.sol
+++ test/L2GovernanceFactory.t.sol
@@ -10,19 +10,26 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 contract L2GovernanceFactoryTest is Test {
-    address[] addressArrayStub = [address(777), address(888)];
-    address owner = address(2323);
-    uint256 l2MinTimelockDelay = 42;
+    // token
+    address l2TokenOwner = address(2);
     address l1Token = address(1);
     uint256 l2TokenInitialSupply = 43;
-    address l2TokenOwner = address(2);
-    address[] l2UpgradeExecutors;
+
+    // timelock
+    uint256 l2MinTimelockDelay = 42;
+
+    // govs
     uint256 votingPeriod = 44;
     uint256 votingDelay = 45;
     uint256 coreQuorumThreshold = 4;
     uint256 treasuryQuorumThreshold = 3;
     uint256 proposalThreshold = 5e6;
-    uint64 minPeriodAfterQuorum = 42;
+    uint64 minPeriodAfterQuorum = 41;
+
+    address[] l2UpgradeExecutors; // TODO
+    address[] addressArrayStub = [address(777), address(888)];
+
+    address someRando = address(3);
 
     function deploy()
         public
@@ -67,6 +74,45 @@ contract L2GovernanceFactoryTest is Test {
         return (token, coreGov, coreTimelock, treasuryGov, treasuryTimelock, proxyAdmin, executor);
     }
 
+    function testDeplyPermisson()
+        public
+        returns (
+            L2ArbitrumToken token,
+            L2ArbitrumGovernor coreGov,
+            ArbitrumTimelock coreTimelock,
+            L2ArbitrumGovernor treasuryGov,
+            ArbitrumTimelock treasuryTimelock,
+            ProxyAdmin proxyAdmin,
+            UpgradeExecutor executor
+        )
+    {
+        address[] memory l2UpgradeExecutors; // DG: TODO should be security council and l1 timelock alias?
+        L2GovernanceFactory l2GovernanceFactory = new L2GovernanceFactory();
+        vm.prank(someRando);
+        vm.expectRevert("NOT_DEPLOYER");
+        (
+            L2ArbitrumToken token,
+            L2ArbitrumGovernor coreGov,
+            L2ArbitrumGovernor treasuryGov,
+            ProxyAdmin proxyAdmin,
+            UpgradeExecutor executor
+        ) = l2GovernanceFactory.deploy(
+            DeployCoreParams({
+                _l2MinTimelockDelay: l2MinTimelockDelay,
+                _l1Token: l1Token,
+                _l2TokenInitialSupply: l2TokenInitialSupply,
+                _l2TokenOwner: l2TokenOwner,
+                _l2UpgradeExecutors: l2UpgradeExecutors,
+                _votingPeriod: votingPeriod,
+                _votingDelay: votingDelay,
+                _coreQuorumThreshold: coreQuorumThreshold,
+                _treasuryQuorumThreshold: treasuryQuorumThreshold,
+                _proposalThreshold: proposalThreshold,
+                _minPeriodAfterQuorum: minPeriodAfterQuorum
+            })
+        );
+    }
+
     function testContractsDeployed() external {
         (
             L2ArbitrumToken token,
@@ -100,22 +146,57 @@ contract L2GovernanceFactoryTest is Test {
         token.initialize(l1Token, l2TokenInitialSupply, l1Token);
 
         vm.expectRevert("Initializable: contract is already initialized");
-        gov.initialize(token, timelock, owner, 1, 1, 1, 1, 1);
+        gov.initialize(token, timelock, someRando, 1, 1, 1, 1, 1);
+
+        vm.expectRevert("Initializable: contract is already initialized");
+        treasuryGov.initialize(token, timelock, someRando, 1, 1, 1, 1, 1);
 
         vm.expectRevert("Initializable: contract is already initialized");
         timelock.initialize(1, addressArrayStub, addressArrayStub);
 
         vm.expectRevert("Initializable: contract is already initialized");
+        treasuryTimelock.initialize(1, addressArrayStub, addressArrayStub);
+
+        vm.expectRevert("Initializable: contract is already initialized");
         address[] memory addresses = new address[](1);
-        addresses[0] = owner;
+        addresses[0] = someRando;
         upgradeExecutor.initialize(address(upgradeExecutor), addresses);
+    }
+
+    function testSanityCheckValues() external {
+        (
+            L2ArbitrumToken token,
+            L2ArbitrumGovernor gov,
+            ArbitrumTimelock coreTimelock,
+            L2ArbitrumGovernor treasuryGov,
+            ArbitrumTimelock treasuryTimelock,
+            ProxyAdmin proxyAdmin,
+            UpgradeExecutor upgradeExecutor
+        ) = deploy();
+        assertEq(token.owner(), l2TokenOwner, "token.owner()");
+        assertEq(token.l1Address(), l1Token, "token.l1Address()");
+        assertEq(token.totalSupply(), l2TokenInitialSupply, "token.totalSupply()");
+
+        assertEq(coreTimelock.getMinDelay(), l2MinTimelockDelay, "coreTimelock.getMinDelay()");
+        assertEq(treasuryTimelock.getMinDelay(), 0, "treasuryTimelock.minDelay() zero");
+
+        assertEq(gov.votingPeriod(), votingPeriod, "gov.votingPeriod()");
+        assertEq(treasuryGov.votingPeriod(), votingPeriod, "treasuryGov.votingPeriod()");
+        assertEq(gov.votingDelay(), votingDelay, "gov.votingDelay()");
+        assertEq(treasuryGov.votingDelay(), votingDelay, "treasuryGov.votingDelay()");
+
+        assertEq(gov.quorumNumerator(), coreQuorumThreshold, "");
+        assertEq(treasuryGov.quorumNumerator(), treasuryQuorumThreshold, "");
+
+        assertEq(gov.proposalThreshold(), proposalThreshold, "");
+        assertEq(treasuryGov.proposalThreshold(), proposalThreshold, "");
+
+        assertEq(gov.lateQuorumVoteExtension(), minPeriodAfterQuorum, "");
+        assertEq(treasuryGov.lateQuorumVoteExtension(), minPeriodAfterQuorum, "");
     }
 }
 
 /**
  * Test TODOs:
- * - testContractsInitialized: check treasury contracts
- * - Sanity checks - contracts init with expected values
- * - Only contract deployer can call deploy
  * - MainnetL2GovernanceFactory: can't call deploy, mainnetDeploy works
  */
