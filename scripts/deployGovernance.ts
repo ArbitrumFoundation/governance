33fa375ab0b81d9b2fbb13e8ea62dd0582e9d96e
Goran Vladika <goran.vladika@gmail.com>
2022-11-17 13:24:07 +0100

    Deploy and init L1 governance

--- scripts/deployGovernance.ts
+++ scripts/deployGovernance.ts
@@ -1,3 +1,4 @@
+import { getL2Network } from "@arbitrum/sdk";
 import { Wallet } from "ethers";
 import { parseEther } from "ethers/lib/utils";
 import { testSetup } from "../test-ts/testSetup";
@@ -5,15 +6,20 @@ import {
   ArbitrumTimelock,
   ArbitrumTimelock__factory,
   FixedDelegateErc20Wallet__factory,
+  L1GovernanceFactory__factory,
   L2ArbitrumGovernor__factory,
   L2ArbitrumToken__factory,
   L2GovernanceFactory__factory,
   UpgradeExecutor__factory,
 } from "../typechain-types";
+import { DeployedEventObject as L2DeployedEventObject } from "../typechain-types/src/L2GovernanceFactory";
 
 const deployGovernance = async (): Promise<ArbitrumTimelock> => {
   console.log("Get deployers and signers");
-  const { l2Deployer, l2Signer } = await testSetup();
+  const { l2Deployer, l2Signer, l1Deployer } = await testSetup();
+
+  console.log("Deploy L1 logic contracts");
+  const l1UpgradeExecutorLogic = await new UpgradeExecutor__factory(l1Deployer).deploy();
 
   console.log("Deploy L2 logic contracts");
   const timelockLogic = await new ArbitrumTimelock__factory(l2Deployer).deploy();
@@ -22,7 +28,8 @@ const deployGovernance = async (): Promise<ArbitrumTimelock> => {
   const l2TokenLogic = await new L2ArbitrumToken__factory(l2Deployer).deploy();
   const upgradeExecutor = await new UpgradeExecutor__factory(l2Deployer).deploy();
 
-  console.log("Deploy L2 governance factory");
+  // step 1
+  console.log("Deploy and init L2 governance");
   const l2GovernanceFactory = await new L2GovernanceFactory__factory(l2Deployer).deploy(
     timelockLogic.address,
     governorLogic.address,
@@ -33,30 +40,48 @@ const deployGovernance = async (): Promise<ArbitrumTimelock> => {
     upgradeExecutor.address
   );
 
-  console.log("Initialize L2 governance");
   const initialSupply = parseEther("1");
   const l2TimeLockDelay = 7;
   const l2SignerAddr = await l2Signer.getAddress();
-  // we use a non zero dummy address for the l1 token
-  // it doesnt exist yet but we plan to upgrade the l2 token contract add this address
   const l1TokenAddress = "0x0000000000000000000000000000000000000001";
   const sevenSecurityCouncil = Wallet.createRandom();
-  await l2GovernanceFactory.deployStep1(
-    {
-      _l2MinTimelockDelay: l2TimeLockDelay,
-      _l2TokenInitialSupply: initialSupply,
-      _upgradeProposer: sevenSecurityCouncil.address,
-      _coreQuorumThreshold: 5,
-      _l1Token: l1TokenAddress,
-      _treasuryQuorumThreshold: 3,
-      _proposalThreshold: 100,
-      _votingDelay: 10,
-      _votingPeriod: 10,
-      _minPeriodAfterQuorum: 1,
-      _l2InitialSupplyRecipient: l2SignerAddr,
-    },
 
-    { gasLimit: 30000000 }
+  const l2GovDeployReceipt = await (
+    await l2GovernanceFactory.deployStep1(
+      {
+        _l2MinTimelockDelay: l2TimeLockDelay,
+        _l2TokenInitialSupply: initialSupply,
+        _upgradeProposer: sevenSecurityCouncil.address,
+        _coreQuorumThreshold: 5,
+        _l1Token: l1TokenAddress,
+        _treasuryQuorumThreshold: 3,
+        _proposalThreshold: 100,
+        _votingDelay: 10,
+        _votingPeriod: 10,
+        _minPeriodAfterQuorum: 1,
+        _l2InitialSupplyRecipient: l2SignerAddr,
+      },
+
+      { gasLimit: 30000000 }
+    )
+  ).wait();
+
+  const l2DeployResult = l2GovDeployReceipt.events?.filter(
+    (e) => e.topics[0] === l2GovernanceFactory.interface.getEventTopic("Deployed")
+  )[0].args as unknown as L2DeployedEventObject;
+
+  // step 2
+  console.log("Deploy and init L1 governance");
+  const l1TimeLockDelay = 5;
+  const l1SecurityCouncil = Wallet.createRandom();
+  const l2Network = await getL2Network(l2Deployer);
+  const l1GovernanceFactory = await new L1GovernanceFactory__factory(l1Deployer).deploy();
+  await l1GovernanceFactory.deployStep2(
+    l1UpgradeExecutorLogic.address,
+    l1TimeLockDelay,
+    l2Network.ethBridge.inbox,
+    l2DeployResult.coreTimelock,
+    l1SecurityCouncil.address
   );
 
   return timelockLogic;
@@ -66,7 +91,6 @@ const verifyDeployment = async (timelockLogic: string): Promise<Boolean> => {
   const { l2Deployer } = await testSetup();
 
   let code = await l2Deployer.provider?.getCode(timelockLogic);
-  console.log(code);
   if (code == "0x") {
     return false;
   }
