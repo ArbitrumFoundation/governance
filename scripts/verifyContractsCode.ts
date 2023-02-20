a75da869332ed3680606339850cbe558d9e2cb91
Goran Vladika <goran.vladika@gmail.com>
2023-02-20 12:23:26 +0100

    Add arb deployer to env vars

--- scripts/verifyContractsCode.ts
+++ scripts/verifyContractsCode.ts
@@ -1,4 +1,9 @@
-import { DeployProgressCache, getDeployersAndConfig, loadDeployedContracts } from "./providerSetup";
+import {
+  DeployProgressCache,
+  getDeployersAndConfig,
+  isDeployingToNova,
+  loadDeployedContracts,
+} from "./providerSetup";
 import { exec } from "child_process";
 import { ethers } from "ethers";
 import { DeployerConfig } from "./deployerConfig";
@@ -268,32 +273,47 @@ class ContractVerifier {
   }
 }
 
+export const envVars = {
+  arbApiKey: process.env["ARBISCAN_API_KEY"] as string,
+  ethApiKey: process.env["ETHERSCAN_API_KEY"] as string,
+  novaApiKey: process.env["NOVA_ARBISCAN_API_KEY"] as string,
+  arbDeployer: process.env["ARB_DEPLOYER"] as string,
+};
+
+const checkEnvVars = (conf: typeof envVars) => {
+  if (conf.arbApiKey == undefined) throw new Error("Missing ARBISCAN_API_KEY in env vars");
+  if (conf.ethApiKey == undefined) throw new Error("Missing ETHERSCAN_API_KEY in env vars");
+  if (isDeployingToNova()) {
+    if (conf.novaApiKey == undefined) throw new Error("Missing NOVA_ARBISCAN_API_KEY in env vars");
+  }
+  if (conf.arbDeployer == undefined) throw new Error("Missing ARB_DEPLOYER in env vars");
+};
+
 async function main() {
   console.log("Start verifying contracts...");
 
+  checkEnvVars(envVars);
   const { arbNetwork, novaNetwork, deployerConfig } = await getDeployersAndConfig();
-
   const deployedContracts: DeployProgressCache = loadDeployedContracts();
 
   const arbVerifier = new ContractVerifier(
     arbNetwork.chainID,
-    process.env["ARBISCAN_API_KEY"] as string,
+    envVars.arbApiKey,
     deployedContracts
   );
-  const arbDeployer = "0x0000000000000000000000000000000000000000"; // TODO - fill in
-  await arbVerifier.verifyArbContracts(deployerConfig, arbDeployer);
+  await arbVerifier.verifyArbContracts(deployerConfig, envVars.arbDeployer);
 
   const ethVerifier = new ContractVerifier(
     arbNetwork.partnerChainID,
-    process.env["ETHERSCAN_API_KEY"] as string,
+    envVars.ethApiKey,
     deployedContracts
   );
   await ethVerifier.verifyEthContracts();
 
-  if (arbNetwork.partnerChainID == 1) {
+  if (isDeployingToNova()) {
     const novaVerifier = new ContractVerifier(
       novaNetwork.chainID,
-      process.env["NOVA_ARBISCAN_API_KEY"] as string,
+      envVars.novaApiKey,
       deployedContracts
     );
     await novaVerifier.verifyNovaContracts();
