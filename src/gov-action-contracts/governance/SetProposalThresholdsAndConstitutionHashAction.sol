1622bcd67fefbc7f723d546810fbae9c6117d3d0
dzgoldman <dzgoldman@wesleyan.edu>
2023-04-18 15:11:44 -0400

    use internal function, not lib, in action; set constitution hash

--- src/gov-action-contracts/governance/SetProposalThresholdsAndConstitutionHashAction.sol
+++ src/gov-action-contracts/governance/SetProposalThresholdsAndConstitutionHashAction.sol
@@ -2,13 +2,13 @@
 pragma solidity 0.8.16;
 
 import "../address-registries/L2AddressRegistry.sol";
-import "./SetProposalThresholdLib.sol";
 
 /// @notice Governance action for AIP 1.2 https://forum.arbitrum.foundation/t/proposal-aip-1-2-foundation-and-dao-governance/13362/22
 contract SetProposalThresholdsAndConstitutionHashAction {
     IL2AddressRegistry public immutable l2GovAddressRegistry;
 
-    bytes32 public constant newConstitutionHash = bytes32("0xTODO"); // TODO: update value
+    bytes32 public constant newConstitutionHash =
+        bytes32(0x44618e85660b81480dc2e7296746216942fa5e5c6ad494bd3f8240e1bbdcdae4);
     uint256 public constant newProposalThreshold = 1_000_000 ether;
 
     constructor(IL2AddressRegistry _l2GovAddressRegistry) {
@@ -17,14 +17,14 @@ contract SetProposalThresholdsAndConstitutionHashAction {
 
     function perform() external {
         IL2ArbitrumGoverner coreGov = l2GovAddressRegistry.coreGov();
-        SetProposalThresholdLib.setProposalThreshold(coreGov, newProposalThreshold);
+        setProposalThreshold(coreGov, newProposalThreshold);
         require(
             coreGov.proposalThreshold() == newProposalThreshold,
             "SetProposalThresholdsAndConstitutionHashAction: core governer proposal threshold not set"
         );
 
         IL2ArbitrumGoverner treasuryGov = l2GovAddressRegistry.treasuryGov();
-        SetProposalThresholdLib.setProposalThreshold(treasuryGov, newProposalThreshold);
+        setProposalThreshold(treasuryGov, newProposalThreshold);
         require(
             treasuryGov.proposalThreshold() == newProposalThreshold,
             "SetProposalThresholdsAndConstitutionHashAction: treasury governer proposal threshold not set"
@@ -37,5 +37,12 @@ contract SetProposalThresholdsAndConstitutionHashAction {
             arbitrumDaoConstitution.constitutionHash() == newConstitutionHash,
             "SetProposalThresholdsAndConstitutionHashAction: new constitution hash not set"
         );
+    }
+
+    function setProposalThreshold(IL2ArbitrumGoverner gov, uint256 newProposalThreshold) internal {
+        bytes memory setProposalThresholdCalldata = abi.encodeWithSelector(
+            IL2ArbitrumGoverner.setProposalThreshold.selector, newProposalThreshold
+        );
+        gov.relay(address(gov), 0, setProposalThresholdCalldata);
     }
 }
