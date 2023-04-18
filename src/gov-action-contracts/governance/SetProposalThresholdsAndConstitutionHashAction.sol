e51492385c4e3bb4c54d65c6ffbc0229c2465dfb
Daniel Goldman <dzgoldman@wesleyan.edu>
2023-04-18 15:00:20 -0400

    Update src/gov-action-contracts/governance/SetProposalThresholdsAndConstitutionHashAction.sol
    Co-authored-by: fredlacs <32464905+fredlacs@users.noreply.github.com>

--- src/gov-action-contracts/governance/SetProposalThresholdsAndConstitutionHashAction.sol
+++ src/gov-action-contracts/governance/SetProposalThresholdsAndConstitutionHashAction.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.16;
 import "../address-registries/L2AddressRegistry.sol";
 import "./SetProposalThresholdLib.sol";
 
-// @notice Governance action for AIP 1.2 https://forum.arbitrum.foundation/t/proposal-aip-1-2-foundation-and-dao-governance/13362/22
+/// @notice Governance action for AIP 1.2 https://forum.arbitrum.foundation/t/proposal-aip-1-2-foundation-and-dao-governance/13362/22
 contract SetProposalThresholdsAndConstitutionHashAction {
     IL2AddressRegistry public immutable l2GovAddressRegistry;
 
