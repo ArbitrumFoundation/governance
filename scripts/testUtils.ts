a2a9c8d01024a9888036eac167116a92a2d595f2
dzgoldman <dzgoldman@wesleyan.edu>
2023-04-20 15:22:46 -0400

    fix missing return statement

--- scripts/testUtils.ts
+++ scripts/testUtils.ts
@@ -81,7 +81,7 @@ export async function getProxyOwner(contractAddress: string, provider: Provider)
  * @returns
  */
  export async function getProxyImplementation(contractAddress: string, provider: Provider) {
-  _getAddressAtStorageSlot(contractAddress, provider, "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc")
+  return _getAddressAtStorageSlot(contractAddress, provider, "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc")
 }
 
 export type Recipients = { readonly [key: string]: BigNumber };
