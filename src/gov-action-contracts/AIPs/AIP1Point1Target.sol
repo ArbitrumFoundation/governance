320d7a45601a74c6b9535d6aa076846700c83d1a
dzgoldman <dzgoldman@wesleyan.edu>
2023-05-16 14:48:20 -0400

    include arbitrum foundation wallet address in AIP1Point1Target event

--- src/gov-action-contracts/AIPs/AIP1Point1Target.sol
+++ src/gov-action-contracts/AIPs/AIP1Point1Target.sol
@@ -8,18 +8,21 @@ pragma solidity 0.8.16;
 /// @dev note that this is not a "Gov Action" contract and thus does not conform to that standard.
 contract AIP1Point1Target {
     address public treasuryTimelock;
+    address public arbitrumFoundationWallet;
+
     bool public passed = false;
 
-    event AIP1Point1Passed();
+    event AIP1Point1Passed(address arbitrumFoundationWallet);
 
-    constructor(address _treasuryTimelock) {
+    constructor(address _treasuryTimelock, address _arbitrumFoundationWallet) {
         treasuryTimelock = _treasuryTimelock;
+        arbitrumFoundationWallet = _arbitrumFoundationWallet;
     }
 
     function effectuate() external {
         require(msg.sender == treasuryTimelock, "AIP1Point1Target: only from treasury timelock");
         require(!passed, "AIP1Point1Target: already passed");
         passed = true;
-        emit AIP1Point1Passed();
+        emit AIP1Point1Passed(arbitrumFoundationWallet);
     }
 }
