8005b3eed62ae0dca4ce03c71cd848b8e98e718b
dzgoldman <dzgoldman@wesleyan.edu>
2023-05-16 12:38:53 -0400

    add aip-1.1 target contract

---
+++ src/gov-action-contracts/AIPs/AIP1Point1Target.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.16;
+
+/// @notice contract for AIP-1.1 https://forum.arbitrum.foundation/t/proposal-aip-1-1-lockup-budget-transparency/13360.
+///  AIP-1.1 Is a non-constitutional proposal as per the constitution and thus is to be put to an on-chain vote; however,
+/// it technically requires no on-chain execution. This contract is to be used as its on-chain target as a formality
+/// and for bookkeeping.
+/// @dev note that this is not a "Gov Action" contract and thus does not conform to that standard.
+contract AIP1Point1Target {
+    address public treasuryTimelock;
+    bool public passed = false;
+
+    event AIP1Point1Passed();
+
+    constructor(address _treasuryTimelock) {
+        treasuryTimelock = _treasuryTimelock;
+    }
+
+    function effectuate() {
+        require(msg.sender == treasuryTimelock, "AIP1Point1Target: only from treasury timelock");
+        require(!passed, "AIP1Point1Target: already passed");
+        passed = true;
+        emit AIP1Point1Passed();
+    }
+}
