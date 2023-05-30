b6dc0cb6192e0502d94a6bfd649c3e85941e62c6
dzgoldman <dzgoldman@wesleyan.edu>
2023-05-30 12:50:04 -0400

    fix typo

---
+++ src/security-council-mgmt/factories/L1SecurityCouncilMgmtFactory.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./SecurityCouncilUpgradeExecutorFactory.sol";
+
+/**
+ * @title L1SecurityCouncilMgmtFactory
+ * @notice Factory for deploying L1SecurityCouncilMgmt contracts.
+ */
+
+ contract L1SecurityCouncilMgmtFactory {
+    
+ }
