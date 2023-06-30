c1694f9ad568eb4b0254463ff9c1f1bb26c5d851
Henry <11198460+godzillaba@users.noreply.github.com>
2023-06-30 15:42:21 -0400

    rename candidate -> contender

--- src/security-council-mgmt/governors/modules/SecurityCouncilNomineeElectionGovernorCountingUpgradeable.sol
+++ src/security-council-mgmt/governors/modules/SecurityCouncilNomineeElectionGovernorCountingUpgradeable.sol
@@ -79,12 +79,12 @@ abstract contract SecurityCouncilNomineeElectionGovernorCountingUpgradeable is
     }
 
     /// @dev This function is responsible for counting votes when they are cast.
-    ///      If this vote pushes the candidate over the line, then the candidate is added to the nominees
+    ///      If this vote pushes the contender over the line, then the contender is added to the nominees
     ///      and only the necessary amount of votes will be deducted from the voter.
     /// @param proposalId the id of the proposal
     /// @param account the account that is casting the vote
     /// @param weight the amount of vote that account held at time of snapshot
-    /// @param params abi encoded (candidate, votes) where votes is the amount of votes the account is using for this candidate
+    /// @param params abi encoded (contender, votes) where votes is the amount of votes the account is using for this contender
     function _countVote(
         uint256 proposalId,
         address account,
@@ -92,17 +92,17 @@ abstract contract SecurityCouncilNomineeElectionGovernorCountingUpgradeable is
         uint256 weight,
         bytes memory params
     ) internal virtual override {
-        // let's say params is (address candidate, uint256 votes)
-        (address candidate, uint256 votes) = abi.decode(params, (address, uint256));
+        // let's say params is (address contender, uint256 votes)
+        (address contender, uint256 votes) = abi.decode(params, (address, uint256));
 
         require(
-            _isContender(proposalId, candidate),
-            "SecurityCouncilNomineeElectionGovernorCountingUpgradeable: Candidate is not eligible"
+            _isContender(proposalId, contender),
+            "SecurityCouncilNomineeElectionGovernorCountingUpgradeable: Contender is not eligible"
         );
 
         require(
-            !isNominee(proposalId, candidate),
-            "SecurityCouncilNomineeElectionGovernorCountingUpgradeable: Candidate already has enough votes"
+            !isNominee(proposalId, contender),
+            "SecurityCouncilNomineeElectionGovernorCountingUpgradeable: Contender already has enough votes"
         );
 
         NomineeElectionState storage election = _elections[proposalId];
@@ -113,41 +113,41 @@ abstract contract SecurityCouncilNomineeElectionGovernorCountingUpgradeable is
             "SecurityCouncilNomineeElectionGovernorCountingUpgradeable: Not enough tokens to cast this vote"
         );
 
-        uint256 prevVotesReceived = election.votesReceived[candidate];
+        uint256 prevVotesReceived = election.votesReceived[contender];
         uint256 votesThreshold = quorum(proposalSnapshot(proposalId));
 
         emit VoteCastForContender({
             proposalId: proposalId,
             voter: account,
-            contender: candidate,
+            contender: contender,
             votes: votes,
             totalUsedVotes: prevVotesUsed + votes,
             totalUsableVotes: weight
         });
 
         if (prevVotesReceived + votes < votesThreshold) {
-            // we didn't push the candidate over the line, so just add the votes
+            // we didn't push the contender over the line, so just add the votes
             election.votesUsed[account] = prevVotesUsed + votes;
-            election.votesReceived[candidate] = prevVotesReceived + votes;
+            election.votesReceived[contender] = prevVotesReceived + votes;
         } else {
-            // we pushed the candidate over the line
-            // we should only give the candidate enough votes to get to the line so that we don't waste votes
+            // we pushed the contender over the line
+            // we should only give the contender enough votes to get to the line so that we don't waste votes
             uint256 votesNeeded = votesThreshold - prevVotesReceived;
 
             election.votesUsed[account] = prevVotesUsed + votesNeeded;
-            election.votesReceived[candidate] = prevVotesReceived + votesNeeded;
+            election.votesReceived[contender] = prevVotesReceived + votesNeeded;
 
-            // push the candidate to the nominees
-            election.nominees.push(candidate);
+            // push the contender to the nominees
+            election.nominees.push(contender);
 
-            emit NewNominee(proposalId, candidate);
+            emit NewNominee(proposalId, contender);
         }
     }
 
-    /// @notice Returns true if the candidate has enough votes to be a nominee
-    function isNominee(uint256 proposalId, address candidate) public view returns (bool) {
+    /// @notice Returns true if the contender has enough votes to be a nominee
+    function isNominee(uint256 proposalId, address contender) public view returns (bool) {
         return
-            _elections[proposalId].votesReceived[candidate] >= quorum(proposalSnapshot(proposalId));
+            _elections[proposalId].votesReceived[contender] >= quorum(proposalSnapshot(proposalId));
     }
 
     /// @notice Returns the number of nominees for a given proposal
