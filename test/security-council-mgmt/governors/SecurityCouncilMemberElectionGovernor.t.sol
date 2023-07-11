54e3fa7ba06c4e145cc0dda62a62926dd82e36b4
Henry <11198460+godzillaba@users.noreply.github.com>
2023-07-11 12:26:27 -0400

    expectCall

--- test/security-council-mgmt/governors/SecurityCouncilMemberElectionGovernor.t.sol
+++ test/security-council-mgmt/governors/SecurityCouncilMemberElectionGovernor.t.sol
@@ -64,6 +64,8 @@ contract SecurityCouncilMemberElectionGovernorTest is Test {
         assertEq(governor.fullWeightDuration(), initParams.fullWeightDuration);
     }
 
+    // test functions defined in SecurityCouncilMemberElectionGovernor
+
     function testRelay() public {
         // make sure relay can only be called by owner
         vm.expectRevert("Ownable: caller is not the owner");
@@ -97,7 +99,28 @@ contract SecurityCouncilMemberElectionGovernorTest is Test {
 
         _propose(0);
     }
-    // todo: test executeElectionResult
+
+    function testExecuteElectionResult() public {
+        // should only be callable by the nominee election governor
+        vm.expectRevert(
+            "SecurityCouncilMemberElectionGovernor: Only the nominee election governor can call this function"
+        );
+        governor.executeElectionResult(new address[](0), Cohort.FIRST);
+
+        // should just call replaceCohort on the security council manager
+        vm.prank(address(initParams.nomineeElectionGovernor));
+        vm.mockCall(address(initParams.securityCouncilManager), "", ""); // make sure calls do not revert
+        vm.expectCall(
+            address(initParams.securityCouncilManager),
+            0,
+            abi.encodeWithSelector(
+                initParams.securityCouncilManager.replaceCohort.selector,
+                new address[](8), // 8 is just a random number
+                Cohort.FIRST
+            )
+        );
+        governor.executeElectionResult(new address[](8), Cohort.FIRST);
+    }
 
 
     function testSelectTopNominees() public {
@@ -121,21 +144,14 @@ contract SecurityCouncilMemberElectionGovernorTest is Test {
         }
     }
 
-
-    // todo: if cleaner, can write a mock contract for the manager
-    // that way we don't have to do the mockCallRevert dance
-    // i do like that the manager is always 0x33 in the traces though, but that's whatever
     function testExecute() public {
         // we need to create a proposal, and vote for 6 nominees
         uint256 proposalId = _createProposalAndVoteForSomeNominees(0, initParams.maxNominees);
 
         // roll to the end of voting
         vm.roll(governor.proposalDeadline(proposalId) + 1);
-
-        // make sure the governor calls the manager with the appropriate calldata
-        // we can do this by trying to call execute before and after using mockCall on the manager
-        // we still need to mock the call to the nominee election governor for cohortOfMostRecentElection
-
+        
+        // governor.execute will call back to the nominee governor to get the cohort, we have to mock here
         vm.mockCall(
             address(initParams.nomineeElectionGovernor),
             abi.encodeWithSelector(
@@ -144,34 +160,18 @@ contract SecurityCouncilMemberElectionGovernorTest is Test {
             abi.encode(Cohort.FIRST)
         );
 
-        // make all calls to the manager revert by default
-        vm.mockCallRevert(address(initParams.securityCouncilManager), "", "");
-
         bytes32 descriptionHash = keccak256(bytes(governor.nomineeElectionIndexToDescription(0)));
 
-        // before mocking manager, should revert
-        // (this just makes sure that execute calls the manager)
-        vm.expectRevert(bytes(""));
-        governor.execute({
-            targets: new address[](1),
-            values: new uint256[](1),
-            calldatas: new bytes[](1),
-            descriptionHash: descriptionHash
-        });
-
-        // mock the call to the manager
-        // calls to the manager with any other calldata will revert
-        vm.mockCall(
+        vm.mockCall(address(initParams.securityCouncilManager), "", "");
+        vm.expectCall(
             address(initParams.securityCouncilManager),
+            0,
             abi.encodeWithSelector(
                 initParams.securityCouncilManager.replaceCohort.selector,
                 governor.topNominees(proposalId),
                 Cohort.FIRST
-            ),
-            abi.encode()
+            )
         );
-
-        // now that we've mocked the call to the manager, should not revert
         governor.execute({
             targets: new address[](1),
             values: new uint256[](1),
@@ -179,32 +179,6 @@ contract SecurityCouncilMemberElectionGovernorTest is Test {
             descriptionHash: descriptionHash
         });
     }
-
-    ////////// SecurityCouncilMemberElectionGovernorCountingUpgradeable tests //////////
-
-    // function testSetFullWeightDurationNumeratorAndDurationDenominator() public {
-    //     // non governor should not be able to call
-    //     vm.expectRevert("Governor: onlyGovernance");
-    //     governor.setFullWeightDurationNumeratorAndDurationDenominator(1, 1);
-
-    //     // governor can call
-    //     vm.prank(address(governor));
-    //     governor.setFullWeightDurationNumeratorAndDurationDenominator(100, 200);
-
-    //     // make sure the values were set
-    //     assertEq(governor.fullWeightDurationNumerator(), 100);
-    //     assertEq(governor.durationDenominator(), 200);
-
-    //     // make sure it reverts when numerator is 0
-    //     vm.prank(address(governor));
-    //     vm.expectRevert("SecurityCouncilMemberElectionGovernorCountingUpgradeable: Full weight duration numerator must be > 0");
-    //     governor.setFullWeightDurationNumeratorAndDurationDenominator(0, 1);
-
-    //     // make sure it reverts when numerator is > denominator
-    //     vm.prank(address(governor));
-    //     vm.expectRevert("SecurityCouncilMemberElectionGovernorCountingUpgradeable: Full weight duration numerator must be <= duration denominator");
-    //     governor.setFullWeightDurationNumeratorAndDurationDenominator(2, 1);
-    // }
 
     function testVotesToWeight() public {
         _propose(0);
@@ -384,6 +358,8 @@ contract SecurityCouncilMemberElectionGovernorTest is Test {
         // make sure the proposal succeeded
         assertTrue(governor.state(proposalId) == IGovernorUpgradeable.ProposalState.Succeeded);
     }
+
+    // helpers
 
     function _voter(uint8 i) internal pure returns (address) {
         return address(uint160(0x1100 + i));
