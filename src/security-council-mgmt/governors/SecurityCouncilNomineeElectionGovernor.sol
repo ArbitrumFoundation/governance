cf49ea3b612224c181e1ce6a512f94f9592279db
Henry <11198460+godzillaba@users.noreply.github.com>
2023-07-17 11:13:22 -0400

    lint

--- src/security-council-mgmt/governors/SecurityCouncilNomineeElectionGovernor.sol
+++ src/security-council-mgmt/governors/SecurityCouncilNomineeElectionGovernor.sol
@@ -123,7 +123,10 @@ contract SecurityCouncilNomineeElectionGovernor is
         if (msg.sender != nomineeVetter) {
             revert OnlyNomineeVetter();
         }
-        if (state(proposalId) != ProposalState.Succeeded || block.number > proposalVettingDeadline(proposalId)) {
+        if (
+            state(proposalId) != ProposalState.Succeeded
+                || block.number > proposalVettingDeadline(proposalId)
+        ) {
             revert ProposalNotInVettingPeriod();
         }
         _;
@@ -201,7 +204,10 @@ contract SecurityCouncilNomineeElectionGovernor is
     /// @notice Allows the nomineeVetter to exclude a noncompliant nominee.
     /// @dev    Can be called only after a nomninee election proposal has "succeeded" (voting has ended) and before the nominee vetting period has ended.
     ///         Will revert if the provided account is not a nominee (had less than the required votes).
-    function excludeNominee(uint256 proposalId, address nominee) external onlyNomineeVetterInVettingPeriod(proposalId) {
+    function excludeNominee(uint256 proposalId, address nominee)
+        external
+        onlyNomineeVetterInVettingPeriod(proposalId)
+    {
         ElectionInfo storage election = _elections[proposalId];
         if (election.isExcluded[nominee]) {
             revert NomineeAlreadyExcluded(nominee);
@@ -219,7 +225,10 @@ contract SecurityCouncilNomineeElectionGovernor is
     /// @notice Allows the nomineeVetter to explicitly include a nominee if there are fewer nominees than the target.
     /// @dev    Can be called only after a proposal has succeeded (voting has ended) and before the nominee vetting period has ended.
     ///         Will revert if the provided account is already a nominee
-    function includeNominee(uint256 proposalId, address account) external onlyNomineeVetterInVettingPeriod(proposalId) {
+    function includeNominee(uint256 proposalId, address account)
+        external
+        onlyNomineeVetterInVettingPeriod(proposalId)
+    {
         if (isNominee(proposalId, account)) {
             revert NomineeAlreadyAdded();
         }
@@ -364,12 +373,22 @@ contract SecurityCouncilNomineeElectionGovernor is
     }
 
     /// @inheritdoc ArbitrumGovernorProposalExpirationUpgradeable
-    function state(uint256 proposalId) public view override(GovernorUpgradeable, ArbitrumGovernorProposalExpirationUpgradeable) returns (ProposalState) {
+    function state(uint256 proposalId)
+        public
+        view
+        override(GovernorUpgradeable, ArbitrumGovernorProposalExpirationUpgradeable)
+        returns (ProposalState)
+    {
         return ArbitrumGovernorProposalExpirationUpgradeable.state(proposalId);
     }
 
     /// @inheritdoc ArbitrumGovernorProposalExpirationUpgradeable
-    function _proposalExpirationCountdownStart(uint256 proposalId) internal view override returns (uint256) {
+    function _proposalExpirationCountdownStart(uint256 proposalId)
+        internal
+        view
+        override
+        returns (uint256)
+    {
         return proposalVettingDeadline(proposalId);
     }
 
