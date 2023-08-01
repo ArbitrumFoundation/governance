a530cca32a045f14fd55520fa1ac9968a19cbbbe
Henry <11198460+godzillaba@users.noreply.github.com>
2023-08-01 13:56:07 -0500

    config comments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 
+// when changing optimizer settings, make sure to also change settings in foundry.toml
 const solidityProfiles = {
   default: {
     version: "0.8.16",
