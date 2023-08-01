60a49d55a5423fafc8b9dd1a601288f07e303c31
fred <fredlacs@gmail.com>
2023-08-01 12:14:41 -0300

    add hardhat forge plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
 
 const solidityProfiles = {
   default: {
