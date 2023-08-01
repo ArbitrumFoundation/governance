e834f44dfdfb611d02c542d9e5ec8f6fd81f0da9
gzeon <im@gzeon.dev>
2023-08-01 23:53:19 +0100

    fix: use imported interface from nitro-contracts

--- src/gov-action-contracts/AIPs/UpgradeArbOS/SetWASMModuleRootAction.sol
+++ src/gov-action-contracts/AIPs/UpgradeArbOS/SetWASMModuleRootAction.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.16;
 
-import "@arbitrum/nitro-contracts/src/rollup/IRollupLogic.sol" as rl;
-import "@arbitrum/nitro-contracts/src/rollup/IRollupCore.sol" as rc;
 import "../../address-registries/L1AddressRegistry.sol";
 
 /// @notice should be included in an operation batch in the L1 timelock along with UpgradeArbOSVersionAction
@@ -16,11 +14,11 @@ contract SetWasmModuleRootAction {
     }
 
     function perform() external {
-        rl.IRollupAdmin(rollup).setWasmModuleRoot(newWasmModuleRoot);
+        IRollupAdmin(rollup).setWasmModuleRoot(newWasmModuleRoot);
 
         // verify:
         require(
-            rc.IRollupCore(rollup).wasmModuleRoot() == newWasmModuleRoot,
+            IRollupCore(rollup).wasmModuleRoot() == newWasmModuleRoot,
             "SetWasmModuleRootAction: wasm module root not set"
         );
     }
