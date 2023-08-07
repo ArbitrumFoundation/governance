be6ec60fe2b67f19511c071ed1cad98a5e721f01
dzgoldman <dzgoldman@wesleyan.edu>
2023-08-07 14:17:08 -0400

    add storage gap to upgradeable contracts

--- src/security-council-mgmt/governors/SecurityCouncilMemberElectionGovernor.sol
+++ src/security-council-mgmt/governors/SecurityCouncilMemberElectionGovernor.sol
@@ -211,4 +211,11 @@ contract SecurityCouncilMemberElectionGovernor is
     {
         revert CastVoteDisabled();
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
--- src/security-council-mgmt/governors/SecurityCouncilMemberRemovalGovernor.sol
+++ src/security-council-mgmt/governors/SecurityCouncilMemberRemovalGovernor.sol
@@ -261,4 +261,11 @@ contract SecurityCouncilMemberRemovalGovernor is
         // to normal changes in the security council membership.
         return ArbitrumGovernorProposalExpirationUpgradeable.state(proposalId);
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
