530f086341098c9ba704da14f3966a98d39e9ff0
Henry <11198460+godzillaba@users.noreply.github.com>
2023-08-09 18:02:54 -0400

    add route builder and rename a variable

--- scripts/security-council-mgmt-deployment/verifyContracts.ts
+++ scripts/security-council-mgmt-deployment/verifyContracts.ts
@@ -1,10 +1,13 @@
-import { GovernanceChainSCMgmtActivationAction__factory, ISecurityCouncilMemberElectionGovernor__factory, KeyValueStore__factory, L1SCMgmtActivationAction__factory, L2SecurityCouncilMgmtFactory__factory, NonGovernanceChainSCMgmtActivationAction__factory, SecurityCouncilManager__factory, SecurityCouncilMemberElectionGovernor__factory, SecurityCouncilMemberRemovalGovernor__factory, SecurityCouncilMemberSyncAction__factory, SecurityCouncilNomineeElectionGovernor__factory, TransparentUpgradeableProxy__factory, UpgradeExecRouteBuilder__factory } from "../../typechain-types";
+import { GovernanceChainSCMgmtActivationAction__factory, ISecurityCouncilMemberElectionGovernor__factory, KeyValueStore__factory, L1ArbitrumTimelock__factory, L1SCMgmtActivationAction__factory, L2SecurityCouncilMgmtFactory__factory, NonGovernanceChainSCMgmtActivationAction__factory, SecurityCouncilManager__factory, SecurityCouncilMemberElectionGovernor__factory, SecurityCouncilMemberRemovalGovernor__factory, SecurityCouncilMemberSyncAction__factory, SecurityCouncilNomineeElectionGovernor__factory, TransparentUpgradeableProxy__factory, UpgradeExecRouteBuilder__factory } from "../../typechain-types";
 import { ContractVerificationConfig, verifyContracts } from "../minimalContractVerifier";
 import { promises as fs} from "fs";
 import { SecurityCouncilManagementDeploymentResult } from "./types";
 import mainnetConfig from "./configs/mainnet";
 import goerliConfig from "./configs/arbgoerli";
 import { assertDefined } from "./utils";
+import { getL2Network } from "@arbitrum/sdk";
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { ethers } from "ethers";
 
 // todo: replace with cli option
 const TESTNET = true;
@@ -26,7 +29,7 @@ const chainIdToApiKey: {[key: number]: string} = {
 async function main() {
   const path = TESTNET ? "files/goerli/scmDeployment.json" : "files/mainnet/scmDeployment.json";
   const scmDeployment = JSON.parse((await fs.readFile(path)).toString()) as SecurityCouncilManagementDeploymentResult;
-  const config = TESTNET ? goerliConfig : mainnetConfig;
+  const deploymentConfig = TESTNET ? goerliConfig : mainnetConfig;
 
   // going in order of scmDeployment
 
@@ -36,39 +39,39 @@ async function main() {
       // gov chain activation contract
       factory: new GovernanceChainSCMgmtActivationAction__factory(),
       contractName: "GovernanceChainSCMgmtActivationAction",
-      chainId: config.govChain.chainID,
-      address: scmDeployment.activationActionContracts[config.govChain.chainID],
+      chainId: deploymentConfig.govChain.chainID,
+      address: scmDeployment.activationActionContracts[deploymentConfig.govChain.chainID],
       constructorArgs: [
-        scmDeployment.emergencyGnosisSafes[config.govChain.chainID],
+        scmDeployment.emergencyGnosisSafes[deploymentConfig.govChain.chainID],
         scmDeployment.nonEmergencyGnosisSafe,
-        config.govChain.prevEmergencySecurityCouncil,
-        config.govChain.prevNonEmergencySecurityCouncil,
-        config.emergencySignerThreshold,
-        config.nonEmergencySignerThreshold,
+        deploymentConfig.govChain.prevEmergencySecurityCouncil,
+        deploymentConfig.govChain.prevNonEmergencySecurityCouncil,
+        deploymentConfig.emergencySignerThreshold,
+        deploymentConfig.nonEmergencySignerThreshold,
         scmDeployment.securityCouncilManager,
-        config.l2AddressRegistry
+        deploymentConfig.l2AddressRegistry
       ],
       foundryProfile: "sec_council_mgmt",
-      etherscanApiKey: chainIdToApiKey[config.govChain.chainID]
+      etherscanApiKey: chainIdToApiKey[deploymentConfig.govChain.chainID]
     },
     {
       // host chain activation contract
       factory: new L1SCMgmtActivationAction__factory(),
       contractName: "L1SCMgmtActivationAction",
-      chainId: config.hostChain.chainID,
-      address: scmDeployment.activationActionContracts[config.hostChain.chainID],
+      chainId: deploymentConfig.hostChain.chainID,
+      address: scmDeployment.activationActionContracts[deploymentConfig.hostChain.chainID],
       constructorArgs: [
-        scmDeployment.emergencyGnosisSafes[config.hostChain.chainID],
-        config.hostChain.prevEmergencySecurityCouncil,
-        config.emergencySignerThreshold,
-        config.l1Executor,
-        config.l1Timelock
+        scmDeployment.emergencyGnosisSafes[deploymentConfig.hostChain.chainID],
+        deploymentConfig.hostChain.prevEmergencySecurityCouncil,
+        deploymentConfig.emergencySignerThreshold,
+        deploymentConfig.l1Executor,
+        deploymentConfig.l1Timelock
       ],
       foundryProfile: "sec_council_mgmt",
-      etherscanApiKey: chainIdToApiKey[config.hostChain.chainID]
+      etherscanApiKey: chainIdToApiKey[deploymentConfig.hostChain.chainID]
     },
     // governed chains activation contracts
-    ...config.governedChains.map((chain) => ({
+    ...deploymentConfig.governedChains.map((chain) => ({
       factory: new NonGovernanceChainSCMgmtActivationAction__factory(),
       contractName: "NonGovernanceChainSCMgmtActivationAction",
       chainId: chain.chainID,
@@ -76,7 +79,7 @@ async function main() {
       constructorArgs: [
         scmDeployment.emergencyGnosisSafes[chain.chainID],
         chain.prevEmergencySecurityCouncil,
-        config.emergencySignerThreshold,
+        deploymentConfig.emergencySignerThreshold,
         chain.upExecLocation
       ],
       foundryProfile: "sec_council_mgmt",
@@ -120,72 +123,112 @@ async function main() {
   const nomineeElectionGovConfig: ContractVerificationConfig = {
     factory: new SecurityCouncilNomineeElectionGovernor__factory(),
     contractName: "SecurityCouncilNomineeElectionGovernor",
-    chainId: config.govChain.chainID,
+    chainId: deploymentConfig.govChain.chainID,
     address: scmDeployment.nomineeElectionGovernorLogic,
     constructorArgs: [],
     foundryProfile: "sec_council_mgmt",
-    etherscanApiKey: chainIdToApiKey[config.govChain.chainID]
+    etherscanApiKey: chainIdToApiKey[deploymentConfig.govChain.chainID]
   };
 
   // nominee election gov proxy (only need to verify one proxy since they all have same deployed bytecode)
   const nomineeElectionGovProxyConfig: ContractVerificationConfig = {
     factory: new TransparentUpgradeableProxy__factory(),
     contractName: "TransparentUpgradeableProxy",
-    chainId: config.govChain.chainID,
+    chainId: deploymentConfig.govChain.chainID,
     address: scmDeployment.nomineeElectionGovernor,
     constructorArgs: [
       scmDeployment.nomineeElectionGovernorLogic,
-      config.l2ProxyAdmin,
+      deploymentConfig.l2ProxyAdmin,
       []
     ],
     foundryProfile: "sec_council_mgmt",
-    etherscanApiKey: chainIdToApiKey[config.govChain.chainID]
+    etherscanApiKey: chainIdToApiKey[deploymentConfig.govChain.chainID]
   };
 
   // member election gov logic
   const memberElectionGovConfig: ContractVerificationConfig = {
     factory: new SecurityCouncilMemberElectionGovernor__factory(),
     contractName: "SecurityCouncilMemberElectionGovernor",
-    chainId: config.govChain.chainID,
+    chainId: deploymentConfig.govChain.chainID,
     address: scmDeployment.memberElectionGovernorLogic,
     constructorArgs: [],
     foundryProfile: "sec_council_mgmt",
-    etherscanApiKey: chainIdToApiKey[config.govChain.chainID]
+    etherscanApiKey: chainIdToApiKey[deploymentConfig.govChain.chainID]
   };
 
   // manager logic
   const managerConfig: ContractVerificationConfig = {
     factory: new SecurityCouncilManager__factory(),
     contractName: "SecurityCouncilManager",
-    chainId: config.govChain.chainID,
+    chainId: deploymentConfig.govChain.chainID,
     address: scmDeployment.securityCouncilManagerLogic,
     constructorArgs: [],
     foundryProfile: "sec_council_mgmt",
-    etherscanApiKey: chainIdToApiKey[config.govChain.chainID]
+    etherscanApiKey: chainIdToApiKey[deploymentConfig.govChain.chainID]
   };
 
   // removal gov logic
   const removalGovConfig: ContractVerificationConfig = {
     factory: new SecurityCouncilMemberRemovalGovernor__factory(),
     contractName: "SecurityCouncilMemberRemovalGovernor",
-    chainId: config.govChain.chainID,
+    chainId: deploymentConfig.govChain.chainID,
     address: scmDeployment.securityCouncilMemberRemoverGovLogic,
     constructorArgs: [],
     foundryProfile: "sec_council_mgmt",
-    etherscanApiKey: chainIdToApiKey[config.govChain.chainID]
+    etherscanApiKey: chainIdToApiKey[deploymentConfig.govChain.chainID]
   };
 
-  // skip route builder since the factory deploys it
+  // route builder
+  const routeBuilderConfig: ContractVerificationConfig = {
+    factory: new UpgradeExecRouteBuilder__factory(),
+    contractName: "UpgradeExecRouteBuilder",
+    chainId: deploymentConfig.govChain.chainID,
+    address: scmDeployment.upgradeExecRouteBuilder,
+    constructorArgs: [
+      [
+        {
+          // (L1) host chain executor
+          chainId: deploymentConfig.hostChain.chainID,
+          location: {
+            inbox: ethers.constants.AddressZero,
+            upgradeExecutor: deploymentConfig.l1Executor,
+          },
+        },
+        {
+          // (L2) gov chain executor
+          chainId: deploymentConfig.govChain.chainID,
+          location: {
+            inbox: (await getL2Network(deploymentConfig.govChain.chainID)).ethBridge.inbox,
+            upgradeExecutor: deploymentConfig.l1Executor,
+          },
+        },
+        // (L2) governed chain executors
+        ...await Promise.all(deploymentConfig.governedChains.map(async (chain) => {
+          return {
+            chainId: chain.chainID,
+            location: {
+              inbox: (await getL2Network(chain.chainID)).ethBridge.inbox,
+              upgradeExecutor: chain.upExecLocation,
+            }
+          }
+        }))
+      ],
+      deploymentConfig.l1Timelock,
+      await L1ArbitrumTimelock__factory.connect(deploymentConfig.l1Timelock, new JsonRpcProvider(assertDefined(process.env.ETH_URL))).getMinDelay()
+    ],
+    foundryProfile: "sec_council_mgmt",
+    etherscanApiKey: chainIdToApiKey[deploymentConfig.govChain.chainID]
+  };
 
   // factory
   const factoryConfig: ContractVerificationConfig = {
     factory: new L2SecurityCouncilMgmtFactory__factory(),
     contractName: "L2SecurityCouncilMgmtFactory",
-    chainId: config.govChain.chainID,
+    chainId: deploymentConfig.govChain.chainID,
     address: scmDeployment.l2SecurityCouncilMgmtFactory,
     constructorArgs: [],
     foundryProfile: "sec_council_mgmt",
-    etherscanApiKey: chainIdToApiKey[config.govChain.chainID]
+    etherscanApiKey: chainIdToApiKey[deploymentConfig.govChain.chainID]
   };
 
   const allConfigs = [
@@ -197,7 +240,8 @@ async function main() {
     memberElectionGovConfig,
     managerConfig,
     removalGovConfig,
-    factoryConfig
+    factoryConfig,
+    routeBuilderConfig
   ];
 
   await verifyContracts(allConfigs);
