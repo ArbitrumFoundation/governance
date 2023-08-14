df2fda6d21115be9888e7717fc354becf5e3930a
dzgoldman <dzgoldman@wesleyan.edu>
2023-08-14 12:35:01 -0400

    deploy gnosis with module

--- test/util/DeployGnosisWithModule.sol
+++ test/util/DeployGnosisWithModule.sol
@@ -3,6 +3,8 @@
 pragma solidity 0.8.16;
 
 import "@gnosis.pm/safe-contracts/contracts/GnosisSafeL2.sol";
+import "@gnosis.pm/safe-contracts/contracts/base/ModuleManager.sol";
+
 import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
 import "forge-std/Test.sol";
 
@@ -16,13 +18,21 @@ contract DeployGnosisWithModule is Test {
 
         GnosisSafeProxy safeProxy = safeProxyFactory.createProxy(address(safeLogic), "0x");
         GnosisSafeL2 safe = GnosisSafeL2(payable(address(safeProxy)));
-        safe.setup(
-            _owners, _threshold, address(0), "0x", address(0), address(0), 0, payable(address(0))
-        );
-        if (_module != address(0)) {
-            vm.prank(address(safe));
-            safe.enableModule(_module);
+          if (_module != address(0)) {
+              safe.setup(
+                _owners, _threshold, address(safe), abi.encodeWithSelector(ModuleManager.enableModule.selector, _module), address(0), address(0), 0, payable(address(0))
+            );
+        } else {
+            safe.setup(
+                _owners, _threshold, address(0), "0x", address(0), address(0), 0, payable(address(0))
+            );
         }
+
+
+        assertTrue(safe.isModuleEnabled(_module), "MODULE_ERROR");
+
+
+      
         return address(safe);
     }
 }
