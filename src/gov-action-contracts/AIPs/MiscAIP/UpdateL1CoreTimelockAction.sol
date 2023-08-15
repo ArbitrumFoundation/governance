69d30750a338f2e6f385fd5480525807e073bc46
fredlacs <32464905+fredlacs@users.noreply.github.com>
2023-08-15 08:29:34 -0300

    Update src/gov-action-contracts/AIPs/MiscAIP/UpdateL1CoreTimelockAction.sol

--- src/gov-action-contracts/AIPs/MiscAIP/UpdateL1CoreTimelockAction.sol
+++ src/gov-action-contracts/AIPs/MiscAIP/UpdateL1CoreTimelockAction.sol
@@ -5,9 +5,9 @@ import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import "../../address-registries/L1AddressRegistry.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-/// @notice Upgdae implement of L1ArbitrumTimelock to fix bug in scheduleBatch;
-/// properly handle batches in which more than one operation.
-/// creates a retryable ticket. Bug is non-critical since workarounds exist
+/// @notice Upgrade implemention of L1ArbitrumTimelock to fix a bug in scheduleBatch function
+/// to properly handle batches in which more than one operation
+/// creates a retryable ticket. The bug is non-critical since workarounds exist
 contract UpdateL1CoreTimelockAction {
     ProxyAdmin public immutable govProxyAdmin;
     L1AddressRegistry public immutable l1AddressRegistry;
