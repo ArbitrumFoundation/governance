d2b8b0820ca9e78bdd38a46fc842d14ba75ff4ac
Chris Buckland <cpbuckland88@gmail.com>
2023-11-02 16:43:04 +0100

    Stringify balance

--- scripts/genNetwork.ts
+++ scripts/genNetwork.ts
@@ -8,6 +8,7 @@ async function main() {
 
   const ethDeployer = getSigner(ethProvider, config.ethKey);
   const arbDeployer = getSigner(arbProvider, config.arbKey);
+  console.log((await arbDeployer.getBalance()).toString())
   
   const { l1Network, l2Network } = await setupNetworks(
     ethDeployer,
