699c308a5482435745e11bad42a6e4da1751c775
dzgoldman <dzgoldman@wesleyan.edu>
2023-12-29 15:18:10 -0500

    add comments

--- src/gov-action-contracts/AIPs/SetSeqMaxTimeVariation/AIPSetSequencerInboxMaxTimeVariationArbOneAction.sol
+++ src/gov-action-contracts/AIPs/SetSeqMaxTimeVariation/AIPSetSequencerInboxMaxTimeVariationArbOneAction.sol
@@ -4,17 +4,18 @@ pragma solidity 0.8.16;
 import "../../address-registries/interfaces.sol";
 import "../../sequencer/SetSequencerInboxMaxTimeVariationAction.sol";
 
+/// @notice Set the future blocks / future seconds to 64 blocks and 64 * 12 seconds so that small L1 reorgs don't cause batches to revert. Delay blocks / delay seconds remain their current values.contract
 contract AIPSetSequencerInboxMaxTimeVariationArbOneAction is
     SetSequencerInboxMaxTimeVariationAction
 {
     // TODO: confirm / finalize values
     constructor()
         SetSequencerInboxMaxTimeVariationAction(
-            ISequencerInboxGetter(0xd514C2b3aaBDBfa10800B9C96dc1eB25427520A0),
-            5760,
-            64,
-            86_400,
-            768
+            ISequencerInboxGetter(0xd514C2b3aaBDBfa10800B9C96dc1eB25427520A0), // Arb One Address Registry
+            5760, // Delay blocks (same as current value)
+            64, // New future blocks value
+            86_400, //  Delay seconds (same as current value)
+            768 // Delay seconds (delay blocks * 12)
         )
     {}
 }
--- src/gov-action-contracts/AIPs/SetSeqMaxTimeVariation/AIPSetSequencerInboxMaxTimeVariationNovaAction.sol
+++ src/gov-action-contracts/AIPs/SetSeqMaxTimeVariation/AIPSetSequencerInboxMaxTimeVariationNovaAction.sol
@@ -4,17 +4,18 @@ pragma solidity 0.8.16;
 import "../../address-registries/interfaces.sol";
 import "../../sequencer/SetSequencerInboxMaxTimeVariationAction.sol";
 
+/// @notice Set the future blocks / future seconds to 64 blocks and 64 * 12 seconds so that small L1 reorgs don't cause batches to revert. Delay blocks / delay seconds remain their current values.
 contract AIPSetSequencerInboxMaxTimeVariationNovaAction is
     SetSequencerInboxMaxTimeVariationAction
 {
     // TODO: confirm / finalize values
     constructor()
         SetSequencerInboxMaxTimeVariationAction(
-            ISequencerInboxGetter(0x2F06643fc2CC18585Ae790b546388F0DE4Ec6635),
-            5760,
-            64,
-            86_400,
-            768
+            ISequencerInboxGetter(0x2F06643fc2CC18585Ae790b546388F0DE4Ec6635), // Nova Address Registry
+            5760, // Delay blocks (same as current value)
+            64, // New future blocks value
+            86_400, //  Delay seconds (same as current value)
+            768 // Delay seconds (delay blocks * 12)
         )
     {}
 }
