a22cb7ac5a8dad6be0cc829ffd2ba18e8d3ccf91
Chris Buckland <cpbuckland88@gmail.com>
2024-01-22 14:17:33 +0000

    Formatting and build updates

--- src/gov-action-contracts/AIPs/AIP4844/AIP4844Action.sol
+++ src/gov-action-contracts/AIPs/AIP4844/AIP4844Action.sol
@@ -11,38 +11,38 @@ interface ISeqInboxPostUpgradeInit {
 
 interface IChallengeManagerUpgradeInit {
     function postUpgradeInit(address _newOsp) external;
-    function osp() external returns(address);
+    function osp() external returns (address);
 }
 
 // CHRIS: TODO: rename with AIP number
- 
+
 /// @notice Upgrades the an arbitrum chain in preparation for 4844
 /// @dev    Identical copies of this contract to be deployed for Arb One and Nova on Ethereum for the 4844 upgrade
-contract AIP4844Action is SetWasmModuleRootAction {
+contract AIP4844Action {
     ProxyAdmin public immutable govProxyAdmin;
     L1AddressRegistry public immutable l1AddressRegistry;
     address public immutable newSequencerInboxImpl;
     address public immutable newChallengeManagerImpl;
     address public immutable newOsp;
+    bytes32 public immutable newWasmModuleRoot;
 
     constructor(
-                L1AddressRegistry _l1AddressRegistry, 
-                bytes32 _newWasmModuleRoot,
-                address _newSequencerInboxImpl,
-                address _newChallengeMangerImpl,
-                ProxyAdmin _govProxyAdmin) {
+        L1AddressRegistry _l1AddressRegistry,
+        bytes32 _newWasmModuleRoot,
+        address _newSequencerInboxImpl,
+        address _newChallengeMangerImpl,
+        ProxyAdmin _govProxyAdmin,
+        address _newOsp
+    ) {
         require(
-            Address.isContract(_11AddressRegistry),
+            Address.isContract(address(_l1AddressRegistry)),
             "AIP4844Action: _11AddressRegistry is not a contract"
         );
-        l1AddressRegistry = _11AddressRegistry;
+        l1AddressRegistry = _l1AddressRegistry;
 
-        require(
-            _newWasmModuleRoot != bytes32(0),
-            "AIP4844Action: _newWasmModuleRoot is empty"
-        );
+        require(_newWasmModuleRoot != bytes32(0), "AIP4844Action: _newWasmModuleRoot is empty");
         newWasmModuleRoot = _newWasmModuleRoot;
-        
+
         require(
             Address.isContract(_newSequencerInboxImpl),
             "AIP4844Action: _newSequencerInboxImpl is not a contract"
@@ -53,47 +53,61 @@ contract AIP4844Action is SetWasmModuleRootAction {
             Address.isContract(_newChallengeMangerImpl),
             "AIP4844Action: _newChallengeMangerImpl is not a contract"
         );
-        newChallengeMangerImpl = _newChallengeMangerImpl;
+        newChallengeManagerImpl = _newChallengeMangerImpl;
 
         require(
-            Address.isContract(_govProxyAdmin),
+            Address.isContract(address(_govProxyAdmin)),
             "AIP4844Action: _govProxyAdmin is not a contract"
         );
         govProxyAdmin = _govProxyAdmin;
+
+        require(Address.isContract(address(_newOsp)), "AIP4844Action: _newOsp is not a contract");
+        newOsp = _newOsp;
     }
 
     function perform() public {
-        IRollupAdmin(rollup).setWasmModuleRoot(newWasmModuleRoot);
+        IRollupCore rollup = l1AddressRegistry.rollup();
+        IRollupAdmin(address(rollup)).setWasmModuleRoot(newWasmModuleRoot);
 
         // verify:
         require(
-            IRollupCore(rollup).wasmModuleRoot() == newWasmModuleRoot,
-            "AIP4844Action: wasm module root not set"
+            rollup.wasmModuleRoot() == newWasmModuleRoot, "AIP4844Action: wasm module root not set"
         );
 
         TransparentUpgradeableProxy sequencerInbox =
             TransparentUpgradeableProxy(payable(address(l1AddressRegistry.sequencerInbox())));
-        (,uint64 futureBlocksBefore,,) = sequencerInbox.maxTimeVariation();
-        govProxyAdmin.upgradeAndCall(sequencerInbox, newSequencerInboxImpl, abi.encodePacked(IPostUpgradeInit.postUpgradeInit.selector));
+        (, uint256 futureBlocksBefore,,) =
+            ISequencerInbox(address(sequencerInbox)).maxTimeVariation();
+        govProxyAdmin.upgradeAndCall(
+            sequencerInbox,
+            newSequencerInboxImpl,
+            abi.encodePacked(ISeqInboxPostUpgradeInit.postUpgradeInit.selector)
+        );
 
         // verify
         require(
             govProxyAdmin.getProxyImplementation(sequencerInbox) == newSequencerInboxImpl,
             "AIP4844Action: new seq inbox implementation set"
         );
-        (,uint64 futureBlocksAfter,,) = sequencerInbox.maxTimeVariation();
+        (, uint256 futureBlocksAfter,,) =
+            ISequencerInbox(address(sequencerInbox)).maxTimeVariation();
         require(
-            futureBlocksBefore != 0 && futureBlocksBefore == futureBlocksAfter, 
+            futureBlocksBefore != 0 && futureBlocksBefore == futureBlocksAfter,
             "AIP4844Action: maxTimeVariation not set"
-        )
+        );
 
         // set the new challenge manager impl
-        TransparentUpgradeableProxy challengeManager =
-            TransparentUpgradeableProxy(payable(address(l1AddressRegistry.rollup().challengeManager())));
-        govProxyAdmin.upgradeAndCall(challengeManager, newChallengeManagerImpl, abi.encodePacked(IChallengeManagerUpgradeInit.postUpgradeInit.selector, newOsp));
+        TransparentUpgradeableProxy challengeManager = TransparentUpgradeableProxy(
+            payable(address(l1AddressRegistry.rollup().challengeManager()))
+        );
+        govProxyAdmin.upgradeAndCall(
+            challengeManager,
+            newChallengeManagerImpl,
+            abi.encodePacked(IChallengeManagerUpgradeInit.postUpgradeInit.selector, newOsp)
+        );
 
         require(
-            IChallengeManagerUpgradeInit(challengeManager).osp() == newOsp, 
+            IChallengeManagerUpgradeInit(address(challengeManager)).osp() == newOsp,
             "AIP4844Action: new OSP not set"
         );
     }
