f5016a359f418cdb4d7e164de87ffa9230872ea9
Daniel Goldman <dzgoldman@wesleyan.edu>
2024-01-26 14:06:28 -0500

    add SetSequencerInboxMaxTimeVariationAction test

---
+++ test/gov-actions/SetSequencerInboxMaxTimeVariationAction.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.16;
+
+import "forge-std/Test.sol";
+
+import "../util/ActionTestBase.sol";
+// import this way to avoid naming collision
+import "../../src/gov-action-contracts/sequencer/SetSequencerInboxMaxTimeVariationAction.sol" as
+    action;
+
+contract SetSequencerInboxMaxTimeVariationAction is Test, ActionTestBase {
+    uint256 public newDelayBlocks = 123;
+    uint256 public newFutureBlocks = 456;
+    uint256 public newDelaySeconds = 789;
+    uint256 public newFutureSeconds = 10_112;
+
+    function testSetMaxTimeVariation() public {
+        action.SetSequencerInboxMaxTimeVariationAction setSequencerInboxMaxTimeVariationAction =
+        new action.SetSequencerInboxMaxTimeVariationAction(
+            addressRegistry, newDelayBlocks, newFutureBlocks, newDelaySeconds, newFutureSeconds
+        );
+        bytes memory callData =
+            abi.encodeWithSelector(action.SetSequencerInboxMaxTimeVariationAction.perform.selector);
+        vm.prank(executor0);
+        ue.execute(address(setSequencerInboxMaxTimeVariationAction), callData);
+        (uint256 delayBlocks, uint256 futureBlocks, uint256 delaySeconds, uint256 futureSeconds) =
+            si.maxTimeVariation();
+        assertEq(delayBlocks, newDelayBlocks, "delay blocks set");
+        assertEq(futureBlocks, newFutureBlocks, "future blocks set");
+        assertEq(delaySeconds, newDelaySeconds, "delay seconds set");
+        assertEq(futureSeconds, newFutureSeconds, "future seconds set");
+    }
+}
