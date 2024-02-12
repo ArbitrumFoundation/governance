5522b2ab9cac14ef2235f8067d01ec46769fb996
gzeon <hng@offchainlabs.com>
2024-02-13 00:05:22 +0800

    docs: suggestions from code review
    Co-authored-by: Daniel Goldman <dzgoldman@wesleyan.edu>

--- src/gov-action-contracts/AIPs/AIP4844/ArbOneAIP4844Action.sol
+++ src/gov-action-contracts/AIPs/AIP4844/ArbOneAIP4844Action.sol
@@ -11,7 +11,7 @@ contract ArbOneAIP4844Action is AIP4844Action {
             0x197290e0f27752789504aab01eff79c46a1f6717b9782621ee8522d2640c4f83, // arbos20 wasm module root
             0x31DA64D19Cd31A19CD09F4070366Fe2144792cf7, // new sequencer inbox impl
             0xE129b8Aa61dF65cBDbAE4345eE3fb40168DfD566, // new challenge manager impl
-            ProxyAdmin(0x554723262467F125Ac9e1cDFa9Ce15cc53822dbD), // l1 gov proxy admin
+            ProxyAdmin(0x554723262467F125Ac9e1cDFa9Ce15cc53822dbD), // L1 core contracts proxy admin for Arb One
             0xC6E1E6dB03c3F475bC760FE20ed93401EC5c4F7e // new one step proof
         )
     {}
--- src/gov-action-contracts/AIPs/AIP4844/NovaAIP4844Action.sol
+++ src/gov-action-contracts/AIPs/AIP4844/NovaAIP4844Action.sol
@@ -11,7 +11,7 @@ contract NovaAIP4844Action is AIP4844Action {
             0x197290e0f27752789504aab01eff79c46a1f6717b9782621ee8522d2640c4f83, // arbos20 wasm module root
             0x31DA64D19Cd31A19CD09F4070366Fe2144792cf7, // new sequencer inbox impl
             0xE129b8Aa61dF65cBDbAE4345eE3fb40168DfD566, // new challenge manager impl
-            ProxyAdmin(0x71D78dC7cCC0e037e12de1E50f5470903ce37148), // l1 gov proxy admin
+            ProxyAdmin(0x71D78dC7cCC0e037e12de1E50f5470903ce37148), //  L1 core contracts proxy admin for Nova
             0xC6E1E6dB03c3F475bC760FE20ed93401EC5c4F7e // new one step proof
         )
     {}
