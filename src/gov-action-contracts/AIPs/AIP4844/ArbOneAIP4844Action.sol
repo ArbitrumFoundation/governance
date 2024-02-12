0fa9870385a79179cfc7d68e95f674515ae2182f
gzeon <im@gzeon.dev>
2024-02-13 00:03:36 +0800

    chore: add arb20 wasm module root

--- src/gov-action-contracts/AIPs/AIP4844/ArbOneAIP4844Action.sol
+++ src/gov-action-contracts/AIPs/AIP4844/ArbOneAIP4844Action.sol
@@ -8,7 +8,7 @@ contract ArbOneAIP4844Action is AIP4844Action {
     constructor()
         AIP4844Action(
             L1AddressRegistry(0xd514C2b3aaBDBfa10800B9C96dc1eB25427520A0), // L1 address registry for Arb One,
-            bytes32(""), // wasm module root  TODO
+            0x197290e0f27752789504aab01eff79c46a1f6717b9782621ee8522d2640c4f83, // arbos20 wasm module root
             0x31DA64D19Cd31A19CD09F4070366Fe2144792cf7, // new sequencer inbox impl
             0xE129b8Aa61dF65cBDbAE4345eE3fb40168DfD566, // new challenge manager impl
             ProxyAdmin(0x554723262467F125Ac9e1cDFa9Ce15cc53822dbD), // l1 gov proxy admin
--- src/gov-action-contracts/AIPs/AIP4844/NovaAIP4844Action.sol
+++ src/gov-action-contracts/AIPs/AIP4844/NovaAIP4844Action.sol
@@ -8,7 +8,7 @@ contract NovaAIP4844Action is AIP4844Action {
     constructor()
         AIP4844Action(
             L1AddressRegistry(0x2F06643fc2CC18585Ae790b546388F0DE4Ec6635), // L1 address registry for Nova,
-            bytes32(""), // wasm module root  TODO
+            0x197290e0f27752789504aab01eff79c46a1f6717b9782621ee8522d2640c4f83, // arbos20 wasm module root
             0x31DA64D19Cd31A19CD09F4070366Fe2144792cf7, // new sequencer inbox impl
             0xE129b8Aa61dF65cBDbAE4345eE3fb40168DfD566, // new challenge manager impl
             ProxyAdmin(0x71D78dC7cCC0e037e12de1E50f5470903ce37148), // l1 gov proxy admin
