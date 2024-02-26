b4578e117e98093d599747e3ca879fe38c86b249
Daniel Goldman <dzgoldman@wesleyan.edu>
2024-02-26 15:08:49 -0500

    remove conditional constitution logic

--- src/gov-action-contracts/governance/SetSCThresholdAndUpdateConstitutionAction.sol
+++ src/gov-action-contracts/governance/SetSCThresholdAndUpdateConstitutionAction.sol
@@ -11,15 +11,13 @@ interface _IGnosisSafe {
 
 ///@notice Set the minimum signing threshold for a security council gnosis safe. Assumes that the safe has the UpgradeExecutor added as a module.
 /// Also conditionally updates constitution dependent on its current hash.
-contract SetSCThresholdAndConditionallyUpdateConstitutionAction {
+contract SetSCThresholdAndUpdateConstitutionAction {
     IGnosisSafe public immutable gnosisSafe;
     uint256 public immutable oldThreshold;
     uint256 public immutable newThreshold;
     IArbitrumDAOConstitution public immutable constitution;
-    bytes32 public immutable oldConstitutionHash1;
-    bytes32 public immutable newConstitutionHash1;
-    bytes32 public immutable oldConstitutionHash2;
-    bytes32 public immutable newConstitutionHash2;
+    bytes32 public immutable oldConstitutionHash;
+    bytes32 public immutable newConstitutionHash;
 
     event ActionPerformed(uint256 newThreshold, bytes32 newConstitutionHash);
 
@@ -28,36 +26,23 @@ contract SetSCThresholdAndConditionallyUpdateConstitutionAction {
         uint256 _oldThreshold,
         uint256 _newThreshold,
         IArbitrumDAOConstitution _constitution,
-        bytes32 _oldConstitutionHash1,
-        bytes32 _newConstitutionHash1,
-        bytes32 _oldConstitutionHash2,
-        bytes32 _newConstitutionHash2
+        bytes32 _oldConstitutionHash,
+        bytes32 _newConstitutionHash
     ) {
         gnosisSafe = _gnosisSafe;
         oldThreshold = _oldThreshold;
         newThreshold = _newThreshold;
         constitution = _constitution;
-        oldConstitutionHash1 = _oldConstitutionHash1;
-        newConstitutionHash1 = _newConstitutionHash1;
-        oldConstitutionHash2 = _oldConstitutionHash2;
-        newConstitutionHash2 = _newConstitutionHash2;
+        oldConstitutionHash = _oldConstitutionHash;
+        newConstitutionHash = _newConstitutionHash;
     }
 
     function perform() external {
-        bytes32[] memory oldConstitutionHashes = new bytes32[](2);
-        oldConstitutionHashes[0] = oldConstitutionHash1;
-        oldConstitutionHashes[1] = oldConstitutionHash2;
-
-        bytes32[] memory newConstitutionHashes = new bytes32[](2);
-        newConstitutionHashes[0] = newConstitutionHash1;
-        newConstitutionHashes[1] = newConstitutionHash2;
-
-        ConstitutionActionLib.conditonallyUpdateConstitutionHash({
-            _constitution: constitution,
-            _oldConstitutionHashes: oldConstitutionHashes,
-            _newConstitutionHashes: newConstitutionHashes
-        });
-
+        require(
+            constitution.constitutionHash() == oldConstitutionHash, "WRONG_OLD_CONSTITUTION_HASH"
+        );
+        constitution.setConstitutionHash(newConstitutionHash);
+        require(constitution.constitutionHash() == newConstitutionHash, "NEW_CONSTUTION_HASH_SET");
         // sanity check old threshold
         require(
             gnosisSafe.getThreshold() == oldThreshold, "SecSCThresholdAction: WRONG_OLD_THRESHOLD"
--- test/gov-actions/AIPIncreaseNonEmergencySCThresholdAction.t.sol
+++ test/gov-actions/AIPIncreaseNonEmergencySCThresholdAction.t.sol
@@ -2,8 +2,7 @@
 pragma solidity 0.8.16;
 
 import "forge-std/Test.sol";
-import
-    "../../src/gov-action-contracts/governance/SetSCThresholdAndConditionallyUpdateConstitutionAction.sol";
+import "../../src/gov-action-contracts/governance/SetSCThresholdAndUpdateConstitutionAction.sol";
 import "../../src/gov-action-contracts/governance/ConstitutionActionLib.sol";
 import "../util/ActionTestBase.sol";
 import "../util/DeployGnosisWithModule.sol";
@@ -24,102 +23,102 @@ contract AIPIncreaseNonEmergencySCThresholdAction is
     bytes32 constHash5 = bytes32("0x5");
 
     address safeAddress;
+    // TODO: outdated tests
+    // function runUpdate(
+    //     bytes32 _initialConstitutionHash,
+    //     bytes32 _oldConstitutionHash1,
+    //     bytes32 _newConstitutionHash1,
+    //     bytes32 _oldConstitutionHash2,
+    //     bytes32 _newConstitutionHash2
+    // ) public {
+    //     safeAddress = deploySafe(owners, oldThreshold, address(arbOneUe));
 
-    function runUpdate(
-        bytes32 _initialConstitutionHash,
-        bytes32 _oldConstitutionHash1,
-        bytes32 _newConstitutionHash1,
-        bytes32 _oldConstitutionHash2,
-        bytes32 _newConstitutionHash2
-    ) public {
-        safeAddress = deploySafe(owners, oldThreshold, address(arbOneUe));
+    //     vm.prank(address(arbOneUe));
+    //     arbitrumDAOConstitution.setConstitutionHash(_initialConstitutionHash);
+    //     assertEq(
+    //         arbitrumDAOConstitution.constitutionHash(),
+    //         _initialConstitutionHash,
+    //         "initial constitution hash set"
+    //     );
 
-        vm.prank(address(arbOneUe));
-        arbitrumDAOConstitution.setConstitutionHash(_initialConstitutionHash);
-        assertEq(
-            arbitrumDAOConstitution.constitutionHash(),
-            _initialConstitutionHash,
-            "initial constitution hash set"
-        );
+    //     address action = address(
+    //         new SetSCThresholdAndConditionallyUpdateConstitutionAction({
+    //             _gnosisSafe: IGnosisSafe(safeAddress),
+    //             _oldThreshold: oldThreshold,
+    //             _newThreshold: newThreshold,
+    //             _constitution: IArbitrumDAOConstitution(address(arbitrumDAOConstitution)),
+    //             _oldConstitutionHash1: _oldConstitutionHash1,
+    //             _newConstitutionHash1: _newConstitutionHash1,
+    //             _oldConstitutionHash2: _oldConstitutionHash2,
+    //             _newConstitutionHash2: _newConstitutionHash2
+    //         })
+    //     );
 
-        address action = address(
-            new SetSCThresholdAndConditionallyUpdateConstitutionAction({
-                _gnosisSafe: IGnosisSafe(safeAddress),
-                _oldThreshold: oldThreshold,
-                _newThreshold: newThreshold,
-                _constitution: IArbitrumDAOConstitution(address(arbitrumDAOConstitution)),
-                _oldConstitutionHash1: _oldConstitutionHash1,
-                _newConstitutionHash1: _newConstitutionHash1,
-                _oldConstitutionHash2: _oldConstitutionHash2,
-                _newConstitutionHash2: _newConstitutionHash2
-            })
-        );
+    //     vm.prank(executor2);
+    //     arbOneUe.execute(
+    //         action,
+    //         abi.encodeWithSelector(
+    //             SetSCThresholdAndConditionallyUpdateConstitutionAction.perform.selector
+    //         )
+    //     );
+    // }
 
-        vm.prank(executor2);
-        arbOneUe.execute(
-            action,
-            abi.encodeWithSelector(
-                SetSCThresholdAndConditionallyUpdateConstitutionAction.perform.selector
-            )
-        );
-    }
+    // function testUpdateInitialHashIsOldHash1() public {
+    //     runUpdate({
+    //         _initialConstitutionHash: constHash1,
+    //         _oldConstitutionHash1: constHash1,
+    //         _newConstitutionHash1: constHash2,
+    //         _oldConstitutionHash2: constHash3,
+    //         _newConstitutionHash2: constHash4
+    //     });
+    //     assertEq(
+    //         arbitrumDAOConstitution.constitutionHash(), constHash2, "proper constitution hash set"
+    //     );
+    //     assertEq(IGnosisSafe(safeAddress).getThreshold(), newThreshold, "new threshold set");
+    // }
 
-    function testUpdateInitialHashIsOldHash1() public {
-        runUpdate({
-            _initialConstitutionHash: constHash1,
-            _oldConstitutionHash1: constHash1,
-            _newConstitutionHash1: constHash2,
-            _oldConstitutionHash2: constHash3,
-            _newConstitutionHash2: constHash4
-        });
-        assertEq(
-            arbitrumDAOConstitution.constitutionHash(), constHash2, "proper constitution hash set"
-        );
-        assertEq(IGnosisSafe(safeAddress).getThreshold(), newThreshold, "new threshold set");
-    }
+    // function testUpdateInitialHashIsOldHash2() public {
+    //     runUpdate({
+    //         _initialConstitutionHash: constHash3,
+    //         _oldConstitutionHash1: constHash1,
+    //         _newConstitutionHash1: constHash2,
+    //         _oldConstitutionHash2: constHash3,
+    //         _newConstitutionHash2: constHash4
+    //     });
+    //     assertEq(
+    //         arbitrumDAOConstitution.constitutionHash(), constHash4, "proper constitution hash set"
+    //     );
+    //     assertEq(IGnosisSafe(safeAddress).getThreshold(), newThreshold, "new threshold set");
+    // }
 
-    function testUpdateInitialHashIsOldHash2() public {
-        runUpdate({
-            _initialConstitutionHash: constHash3,
-            _oldConstitutionHash1: constHash1,
-            _newConstitutionHash1: constHash2,
-            _oldConstitutionHash2: constHash3,
-            _newConstitutionHash2: constHash4
-        });
-        assertEq(
-            arbitrumDAOConstitution.constitutionHash(), constHash4, "proper constitution hash set"
-        );
-        assertEq(IGnosisSafe(safeAddress).getThreshold(), newThreshold, "new threshold set");
-    }
-
-    function testUnfoundConstitutionHash() public {
-        safeAddress = deploySafe(owners, oldThreshold, address(arbOneUe));
-        vm.prank(address(arbOneUe));
-        arbitrumDAOConstitution.setConstitutionHash(constHash1);
-        assertEq(
-            arbitrumDAOConstitution.constitutionHash(), constHash1, "initial constitution hash set"
-        );
-        address action = address(
-            new SetSCThresholdAndConditionallyUpdateConstitutionAction({
-                _gnosisSafe: IGnosisSafe(safeAddress),
-                _oldThreshold: oldThreshold,
-                _newThreshold: newThreshold,
-                _constitution: IArbitrumDAOConstitution(address(arbitrumDAOConstitution)),
-                _oldConstitutionHash1: constHash2,
-                _newConstitutionHash1: constHash3,
-                _oldConstitutionHash2: constHash4,
-                _newConstitutionHash2: constHash5
-            })
-        );
-        vm.expectRevert(
-            abi.encodeWithSelector(ConstitutionActionLib.UnhandledConstitutionHash.selector)
-        );
-        vm.prank(executor2);
-        arbOneUe.execute(
-            action,
-            abi.encodeWithSelector(
-                SetSCThresholdAndConditionallyUpdateConstitutionAction.perform.selector
-            )
-        );
-    }
+    // function testUnfoundConstitutionHash() public {
+    //     safeAddress = deploySafe(owners, oldThreshold, address(arbOneUe));
+    //     vm.prank(address(arbOneUe));
+    //     arbitrumDAOConstitution.setConstitutionHash(constHash1);
+    //     assertEq(
+    //         arbitrumDAOConstitution.constitutionHash(), constHash1, "initial constitution hash set"
+    //     );
+    //     address action = address(
+    //         new SetSCThresholdAndConditionallyUpdateConstitutionAction({
+    //             _gnosisSafe: IGnosisSafe(safeAddress),
+    //             _oldThreshold: oldThreshold,
+    //             _newThreshold: newThreshold,
+    //             _constitution: IArbitrumDAOConstitution(address(arbitrumDAOConstitution)),
+    //             _oldConstitutionHash1: constHash2,
+    //             _newConstitutionHash1: constHash3,
+    //             _oldConstitutionHash2: constHash4,
+    //             _newConstitutionHash2: constHash5
+    //         })
+    //     );
+    //     vm.expectRevert(
+    //         abi.encodeWithSelector(ConstitutionActionLib.UnhandledConstitutionHash.selector)
+    //     );
+    //     vm.prank(executor2);
+    //     arbOneUe.execute(
+    //         action,
+    //         abi.encodeWithSelector(
+    //             SetSCThresholdAndConditionallyUpdateConstitutionAction.perform.selector
+    //         )
+    //     );
+    // }
 }
