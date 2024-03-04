8572b2f8dbbac93c0305ceee0c3c967acec70c12
Daniel Goldman <dzgoldman@wesleyan.edu>
2024-03-04 10:26:15 -0500

    add non-emergency security council proposal creation to propBuilderCli

--- scripts/proposals/buildProposal.ts
+++ scripts/proposals/buildProposal.ts
@@ -1,7 +1,44 @@
 import { Provider } from "@ethersproject/providers";
-import { CoreGovProposal } from "./coreGovProposalInterface";
+import { CoreGovProposal, NonEmergencySCProposal } from "./coreGovProposalInterface";
 import { ArbSys__factory, UpgradeExecRouteBuilder__factory } from "../../typechain-types";
-import { BigNumberish, BytesLike, ethers } from "ethers";
+import { BigNumberish, BytesLike } from "ethers";
+
+async function _getCallDataFromRouteBuilder(
+  provider: Provider,
+  routeBuilderAddress: string,
+  actionChainIds: number[],
+  actionAddresses: string[],
+  timelockSalt: BytesLike,
+  actionValues: BigNumberish[] | undefined,
+  actionDatas: BytesLike[] | undefined,
+  predecessor: BytesLike | undefined
+) {
+  const routeBuilder = UpgradeExecRouteBuilder__factory.connect(routeBuilderAddress, provider);
+  if (actionValues && actionDatas && predecessor) {
+    return (
+      await routeBuilder.createActionRouteData(
+        actionChainIds,
+        actionAddresses,
+        actionValues,
+        actionDatas,
+        predecessor,
+        timelockSalt
+      )
+    )[1];
+  } else if (actionValues || actionDatas || predecessor) {
+    throw new Error(
+      "Custom actionValues, actionDatas and predecessor must all be provided if any are"
+    );
+  } else {
+    return (
+      await routeBuilder.createActionRouteDataWithDefaults(
+        actionChainIds,
+        actionAddresses,
+        timelockSalt
+      )
+    )[1];
+  }
+}
 
 async function _buildProposal(
   description: string,
@@ -9,36 +46,25 @@ async function _buildProposal(
   routeBuilderAddress: string,
   actionChainIds: number[],
   actionAddresses: string[],
+  timelockSalt: BytesLike,
   actionValues: BigNumberish[] | undefined,
   actionDatas: BytesLike[] | undefined,
-  predecessor: BytesLike | undefined,
-  timelockSalt: BytesLike | undefined
+  predecessor: BytesLike | undefined
 ): Promise<CoreGovProposal> {
-  const routeBuilder = UpgradeExecRouteBuilder__factory.connect(
+  let calldata = await _getCallDataFromRouteBuilder(
+    provider,
     routeBuilderAddress,
-    provider
+    actionChainIds,
+    actionAddresses,
+    timelockSalt,
+    actionValues,
+    actionDatas,
+    predecessor
   );
-  
-  let calldata;
 
-  if (actionValues && actionDatas && predecessor && timelockSalt) {
-    [, calldata] = await routeBuilder.createActionRouteData(
-      actionChainIds,
-      actionAddresses,
-      actionValues,
-      actionDatas,
-      predecessor,
-      timelockSalt
-    );
-  }
-  else {
-    [, calldata] = await routeBuilder.createActionRouteDataWithDefaults(
-      actionChainIds,
-      actionAddresses,
-      timelockSalt || ethers.constants.HashZero,
-    );
-  }
-
+  // The route builder encodes the sendTxToL1 call in the calldata it returns.
+  // Proposal submission on the governor has this value passed in as a separate parameter;
+  // so here we decode to retrieve the appropriate calldata.
   const decoded = ArbSys__factory.createInterface().decodeFunctionData("sendTxToL1", calldata);
 
   return {
@@ -58,10 +84,10 @@ export function buildProposalCustom(
   routeBuilderAddress: string,
   actionChainIds: number[],
   actionAddresses: string[],
+  timelockSalt: BytesLike,
   actionValues: BigNumberish[],
   actionDatas: BytesLike[],
-  predecessor: BytesLike,
-  timelockSalt: BytesLike
+  predecessor: BytesLike
 ): Promise<CoreGovProposal> {
   return _buildProposal(
     description,
@@ -69,10 +95,10 @@ export function buildProposalCustom(
     routeBuilderAddress,
     actionChainIds,
     actionAddresses,
+    timelockSalt,
     actionValues,
     actionDatas,
-    predecessor,
-    timelockSalt
+    predecessor
   );
 }
 
@@ -82,7 +108,7 @@ export function buildProposal(
   routeBuilderAddress: string,
   actionChainIds: number[],
   actionAddresses: string[],
-  timelockSalt: BytesLike | undefined = undefined
+  timelockSalt: BytesLike
 ): Promise<CoreGovProposal> {
   return _buildProposal(
     description,
@@ -90,9 +116,42 @@ export function buildProposal(
     routeBuilderAddress,
     actionChainIds,
     actionAddresses,
+    timelockSalt,
     undefined,
     undefined,
-    undefined,
-    timelockSalt
-  )
+    undefined
+  );
+}
+
+export async function buildNonEmergencySecurityCouncilProposal(
+  description: string,
+  provider: Provider,
+  routeBuilderAddress: string,
+  actionChainIds: number[],
+  actionAddresses: string[],
+  timelockSalt: BytesLike,
+  actionValues?: BigNumberish[],
+  actionDatas?: BytesLike[],
+  predecessor?: BytesLike
+): Promise<NonEmergencySCProposal> {
+  let calldata = await _getCallDataFromRouteBuilder(
+    provider,
+    routeBuilderAddress,
+    actionChainIds,
+    actionAddresses,
+    timelockSalt,
+    actionValues,
+    actionDatas,
+    predecessor
+  );
+
+  return {
+    actionChainIds,
+    actionAddresses,
+    description,
+    l2TimelockScheduleArgs: {
+      target: "0x0000000000000000000000000000000000000064", // arb sys address
+      calldata,
+    },
+  };
 }
