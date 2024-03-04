3614916a9cc5de5e8853675625b86eec2390793b
Daniel Goldman <dzgoldman@wesleyan.edu>
2024-03-04 17:50:30 -0500

    add ActionCanExecute contract

--- src/gov-action-contracts/AtlasFees/ArbOneSetAtlasFeesAction.sol
+++ src/gov-action-contracts/AtlasFees/ArbOneSetAtlasFeesAction.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.16;
 
 import "../arb-precompiles/ArbPrecompilesLib.sol";
+import "../util/ActionCanExecute.sol";
 
 interface IArbGasInfo {
     function getMinimumGasPrice() external view returns (uint256);
@@ -12,19 +13,23 @@ contract ArbOneSetAtlasFeesAction {
     uint64 public constant NEW_MIN_BASE_FEE = 0.01 gwei;
     uint64 public constant NEW_L1_REWARD_RATE = 0;
 
-    function perform() external {
-        ArbPrecompilesLib.arbOwner.setMinimumL2BaseFee(NEW_MIN_BASE_FEE);
-        ArbPrecompilesLib.arbOwner.setL1PricingRewardRate(NEW_L1_REWARD_RATE);
+    address public actionCanExecuteAddr = address(0); // TODO
 
-        // verify:
-        IArbGasInfo arbGasInfo = IArbGasInfo(0x000000000000000000000000000000000000006C);
-        require(
-            arbGasInfo.getMinimumGasPrice() == NEW_MIN_BASE_FEE,
-            "ArbOneSetAtlasFeesAction: min L2 gas price"
-        );
-        require(
-            arbGasInfo.getL1RewardRate() == NEW_L1_REWARD_RATE,
-            "ArbOneSetAtlasFeesAction: L1 reward rate"
-        );
+    function perform() external {
+        if (ActionCanExecute(actionCanExecuteAddr).canExecute()) {
+            ArbPrecompilesLib.arbOwner.setMinimumL2BaseFee(NEW_MIN_BASE_FEE);
+            ArbPrecompilesLib.arbOwner.setL1PricingRewardRate(NEW_L1_REWARD_RATE);
+
+            // verify:
+            IArbGasInfo arbGasInfo = IArbGasInfo(0x000000000000000000000000000000000000006C);
+            require(
+                arbGasInfo.getMinimumGasPrice() == NEW_MIN_BASE_FEE,
+                "ArbOneSetAtlasFeesAction: min L2 gas price"
+            );
+            require(
+                arbGasInfo.getL1RewardRate() == NEW_L1_REWARD_RATE,
+                "ArbOneSetAtlasFeesAction: L1 reward rate"
+            );
+        }
     }
 }
