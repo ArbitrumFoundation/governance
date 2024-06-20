596cd80a18dd11a39c7178348146960eb8f44a1c
Henry <11198460+godzillaba@users.noreply.github.com>
2024-06-20 09:54:29 -0400

    add generate.bash

---
+++ scripts/proposals/AtlasFeesAIP/generate.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yarn gen:proposalData --govChainProviderRPC https://arb1.arbitrum.io/rpc \
+    --actionChainIds 42161 42161 \
+    --actionAddresses 0x36D0170D92F66e8949eB276C3AC4FEA64f83704d 0x849E360a247132F961c9CBE95Ba39106c72e1268 \
+    --writeToJsonPath ./scripts/proposals/AtlasFeesAIP/atlais-aip-data.json \
+    --nonEmergencySCproposal true
