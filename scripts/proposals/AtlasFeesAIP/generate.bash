6de926e24b160d75ea43a014d27e19cc07716cae
Henry <11198460+godzillaba@users.noreply.github.com>
2024-06-27 15:59:20 -0400

    typo

--- scripts/proposals/AtlasFeesAIP/generate.bash
+++ scripts/proposals/AtlasFeesAIP/generate.bash
@@ -3,5 +3,5 @@
 yarn gen:proposalData --govChainProviderRPC https://arb1.arbitrum.io/rpc \
     --actionChainIds 42161 42161 \
     --actionAddresses 0x36D0170D92F66e8949eB276C3AC4FEA64f83704d 0x849E360a247132F961c9CBE95Ba39106c72e1268 \
-    --writeToJsonPath ./scripts/proposals/AtlasFeesAIP/atlais-aip-data.json \
+    --writeToJsonPath ./scripts/proposals/AtlasFeesAIP/atlas-aip-data.json \
     --nonEmergencySCproposal true
