59a668c0e9ca57764a04cd88bae6b4d8f7180a76
Henry <11198460+godzillaba@users.noreply.github.com>
2024-07-24 08:07:15 -0400

    Arbos 31 timed upgrade action (#301)
    * feat: UpgradeArbOSVersionAtTimestampAction
    
    * upgrade at timestamp
    
    ---------
    
    Co-authored-by: gzeon <im@gzeon.dev>

--- src/gov-action-contracts/AIPs/AIPArbOS31/SetArbOS31VersionAction.sol
+++ src/gov-action-contracts/AIPs/AIPArbOS31/SetArbOS31VersionAction.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.16;
 
-import "../../arbos-upgrade/UpgradeArbOSVersionAction.sol";
+import "../../arbos-upgrade/UpgradeArbOSVersionAtTimestampAction.sol";
 
 /// @notice Sets the ArbOS version to 31. To be run on an arbitrum chain
 /// @dev    Identical copies of this contract to be deployed on Arb One and Nova for ArbOS31 upgrades
-contract SetArbOS31VersionAction is UpgradeArbOSVersionAction {
-    constructor() UpgradeArbOSVersionAction(uint64(31), 0 /*TODO*/ ) {}
+contract SetArbOS31VersionAction is UpgradeArbOSVersionAtTimestampAction {
+    constructor() UpgradeArbOSVersionAtTimestampAction(uint64(31), 1725382800) {}
 }
