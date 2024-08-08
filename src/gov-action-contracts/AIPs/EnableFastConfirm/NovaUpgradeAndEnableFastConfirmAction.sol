e7ffee080a21a0f66b0992f33f3ab885c6b667f3
gzeon <hng@offchainlabs.com>
2024-08-08 14:58:58 +0800

    feat: v2.1.0 rollup logics on mainnet

--- src/gov-action-contracts/AIPs/EnableFastConfirm/NovaUpgradeAndEnableFastConfirmAction.sol
+++ src/gov-action-contracts/AIPs/EnableFastConfirm/NovaUpgradeAndEnableFastConfirmAction.sol
@@ -8,8 +8,8 @@ contract NovaUpgradeAndEnableFastConfirmAction is UpgradeAndEnableFastConfirmAct
     constructor()
         UpgradeAndEnableFastConfirmAction(
             L1AddressRegistry(0x2F06643fc2CC18585Ae790b546388F0DE4Ec6635),
-            0x0000000000000000000000000000000000000000, // TODO: v2.1.0 RollupAdminLogic
-            0x0000000000000000000000000000000000000000, // TODO: v2.1.0 RollupUserLogic
+            0x2f9491DB1920726d0cFE8AC5F1caC1f730C5dC44, // v2.1.0 RollupAdminLogic
+            0x5c93BAB9Ff2Fa3884b643bd8545C625De0633517, // v2.1.0 RollupUserLogic
             0x0000000000000000000000000000000000000000, // TODO: anyTrustFastConfirmer
             1 // TODO: newMinimumAssertionPeriod in blocks
         )
