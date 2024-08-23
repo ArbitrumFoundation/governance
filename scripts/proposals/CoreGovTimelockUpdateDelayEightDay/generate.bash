7266d243d484f36e36dbc4d863673b03df51a776
Chris Buckland <cpbuckland88@gmail.com>
2024-08-23 15:14:28 +0100

    Updated prop data

--- scripts/proposals/CoreGovTimelockUpdateDelayEightDay/generate.bash
+++ scripts/proposals/CoreGovTimelockUpdateDelayEightDay/generate.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# CoreGovTimelockUpdateDelayEightDayAction 0x17D2448355D6F87F0391B1304A268db9057C72a8
+# CoreGovTimelockUpdateDelayEightDayAction 0x31ab4d23D1D581b61f9D3CE70594BDD2156ea92C
 
 yarn gen:proposalData \
     --govChainProviderRPC https://arb1.arbitrum.io/rpc \
     --actionChainIds 42161 \
-    --actionAddresses 0x17D2448355D6F87F0391B1304A268db9057C72a8 \
+    --actionAddresses 0x31ab4d23D1D581b61f9D3CE70594BDD2156ea92C \
     --writeToJsonPath ./scripts/proposals/CoreGovTimelockUpdateDelayEightDay/CoreGovTimelockUpdateDelayEightDay.json
