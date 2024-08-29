e3582d8b40c57003feaae77176bfd229d6dc01bb
gzeon <im@gzeon.dev>
2024-08-29 15:40:13 +0900

    fix: weekday math

--- src/gov-action-contracts/util/OfficeHoursAction.sol
+++ src/gov-action-contracts/util/OfficeHoursAction.sol
@@ -63,8 +63,8 @@ contract OfficeHoursAction {
         if (block.timestamp < minimumTimestamp) revert MinimumTimestampNotMet();
 
         // Convert timestamp to weekday (1 = Monday, 7 = Sunday)
-        // Addiding 4 because Unix epoch (January 1, 1970) was a Thursday
-        uint256 weekday = ((block.timestamp / 86_400 + 4) % 7);
+        // Addiding 3 because Unix epoch (January 1, 1970) was a Thursday
+        uint256 weekday = ((block.timestamp / 86_400 + 3) % 7 + 1);
         if (weekday < minDayOfWeek || weekday > maxDayOfWeek) revert OutsideOfficeDays();
 
         // This is UTC time, leap seconds are not accounted for
