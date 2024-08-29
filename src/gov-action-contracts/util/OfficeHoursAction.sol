551817faa037c4000e16c9f0e54b2ca1126a4447
gzeon <im@gzeon.dev>
2024-08-29 15:18:30 +0900

    format: forge fmt

--- src/gov-action-contracts/util/OfficeHoursAction.sol
+++ src/gov-action-contracts/util/OfficeHoursAction.sol
@@ -11,7 +11,7 @@ contract OfficeHoursAction {
     uint256 public immutable maxLocalHour;
 
     /// @notice The offset from UTC to local time. e.g. -5 for EST, -4 for EDT
-    int256  public immutable localHourOffset;
+    int256 public immutable localHourOffset;
 
     /// @notice The minimum day of week (inclusive) to execute the action on. 1 = Monday, 7 = Sunday
     /// @dev We assume the range would not cross weekends
@@ -23,10 +23,17 @@ contract OfficeHoursAction {
     /// @notice The minimum timestamp to execute the action on
     uint256 public immutable minimumTimestamp;
 
-    constructor(uint256 _minLocalHour, uint256 _maxLocalHour, int256 _localHourOffset, uint256 _minDayOfWeek, uint256 _maxDayOfWeek, uint256 _minimumTimestamp) {
+    constructor(
+        uint256 _minLocalHour,
+        uint256 _maxLocalHour,
+        int256 _localHourOffset,
+        uint256 _minDayOfWeek,
+        uint256 _maxDayOfWeek,
+        uint256 _minimumTimestamp
+    ) {
         require(_minLocalHour < _maxLocalHour, "Invalid hour range");
         require(_minLocalHour <= 24, "Invalid hour start");
-        require(_maxLocalHour > 0 && _maxLocalHour <= 24 , "Invalid hour end");
+        require(_maxLocalHour > 0 && _maxLocalHour <= 24, "Invalid hour end");
         require(_localHourOffset >= -12 && _localHourOffset <= 14, "Invalid local hour offset");
         require(_minDayOfWeek <= _maxDayOfWeek, "Invalid day of week range");
         require(_minDayOfWeek >= 1 && _minDayOfWeek <= 5, "Invalid day of week start");
@@ -45,14 +52,20 @@ contract OfficeHoursAction {
         require(block.timestamp >= minimumTimestamp, "Cannot execute before minimum timestamp");
 
         // Convert timestamp to weekday (1 = Monday, 7 = Sunday)
-        uint256 weekday = ((block.timestamp / 86400 + 3) % 7) + 1;
-        require(weekday >= minDayOfWeek && weekday <= maxDayOfWeek, "Cannot execute outside the office days");
+        uint256 weekday = ((block.timestamp / 86_400 + 3) % 7) + 1;
+        require(
+            weekday >= minDayOfWeek && weekday <= maxDayOfWeek,
+            "Cannot execute outside the office days"
+        );
 
         // This is UTC time, leap seconds are not accounted for
-        uint256 hoursSinceMidnight = (block.timestamp % 86400) / 3600;
+        uint256 hoursSinceMidnight = (block.timestamp % 86_400) / 3600;
         // Apply offset to convert to local time, also wrap if needed
         uint256 localHour = (hoursSinceMidnight + localHourOffset + 24) % 24;
 
-        require(localHour >= minLocalHour && localHour < maxLocalHour, "Cannot execute outside of office hours");
+        require(
+            localHour >= minLocalHour && localHour < maxLocalHour,
+            "Cannot execute outside of office hours"
+        );
     }
 }
