557b6c4af1eca17bfa1cb9239e7908451059f7e3
Henry <11198460+godzillaba@users.noreply.github.com>
2024-10-16 14:56:29 -0400

    simplify

--- src/gov-action-contracts/AIPs/upgrade-executor-upgrade/UpgradeExecutorUpgradeAction.sol
+++ src/gov-action-contracts/AIPs/upgrade-executor-upgrade/UpgradeExecutorUpgradeAction.sol
@@ -1,21 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.16;
 
-import "@offchainlabs/upgrade-executor/src/UpgradeExecutor.sol";
+import {UpgradeExecutor} from "@offchainlabs/upgrade-executor/src/UpgradeExecutor.sol";
 
 import {
     ProxyAdmin,
     TransparentUpgradeableProxy
 } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Upgrade.sol";
+import {ERC1967Upgrade} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Upgrade.sol";
 
 contract UpgradeExecutorUpgradeAction is ERC1967Upgrade {
-    address public immutable newUpgradeExecutorImplementation;
-
-    constructor() {
-        newUpgradeExecutorImplementation = address(new UpgradeExecutor());
-    }
+    address public immutable newUpgradeExecutorImplementation = address(new UpgradeExecutor());
 
     function perform() external {
         ProxyAdmin proxyAdmin = ProxyAdmin(_getAdmin());
