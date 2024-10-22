e8163949a54ca2280b15d11fd2fa12c16f9eeda6
Henry <11198460+godzillaba@users.noreply.github.com>
2024-10-22 09:38:38 -0400

    deploy exec separately

--- src/gov-action-contracts/AIPs/upgrade-executor-upgrade/UpgradeExecutorUpgradeAction.sol
+++ src/gov-action-contracts/AIPs/upgrade-executor-upgrade/UpgradeExecutorUpgradeAction.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.16;
 
-import {UpgradeExecutor} from "@offchainlabs/upgrade-executor/src/UpgradeExecutor.sol";
+import {UpgradeExecutor} from "@offchainlabs/upgrade-executor/src/UpgradeExecutor.sol"; // todo: deploy UpgradeExecutor separately
 import {
     ProxyAdmin,
     TransparentUpgradeableProxy
 } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 contract UpgradeExecutorUpgradeAction {
-    address public immutable newUpgradeExecutorImplementation;
     ProxyAdmin public immutable proxyAdmin;
+    address public immutable newUpgradeExecutorImplementation;
 
-    constructor(address _proxyAdmin) {
+    constructor(address _proxyAdmin, address _newUpgradeExecutorImplementation) {
         proxyAdmin = ProxyAdmin(_proxyAdmin);
-        newUpgradeExecutorImplementation = address(new UpgradeExecutor());
+        newUpgradeExecutorImplementation = _newUpgradeExecutorImplementation;
     }
 
     function perform() external {
@@ -34,13 +34,28 @@ contract UpgradeExecutorUpgradeAction {
 // L1:   0x5613AF0474EB9c528A34701A5b1662E3C8FA0678
 
 contract ArbOneUpgradeExecutorUpgradeAction is UpgradeExecutorUpgradeAction {
-    constructor() UpgradeExecutorUpgradeAction(0xdb216562328215E010F819B5aBe947bad4ca961e) {}
+    constructor()
+        UpgradeExecutorUpgradeAction(
+            0xdb216562328215E010F819B5aBe947bad4ca961e,
+            address(new UpgradeExecutor()) // todo: deploy UpgradeExecutor separately
+        )
+    {}
 }
 
 contract NovaUpgradeExecutorUpgradeAction is UpgradeExecutorUpgradeAction {
-    constructor() UpgradeExecutorUpgradeAction(0xf58eA15B20983116c21b05c876cc8e6CDAe5C2b9) {}
+    constructor()
+        UpgradeExecutorUpgradeAction(
+            0xf58eA15B20983116c21b05c876cc8e6CDAe5C2b9,
+            address(new UpgradeExecutor()) // todo: deploy UpgradeExecutor separately
+        )
+    {}
 }
 
 contract L1UpgradeExecutorUpgradeAction is UpgradeExecutorUpgradeAction {
-    constructor() UpgradeExecutorUpgradeAction(0x5613AF0474EB9c528A34701A5b1662E3C8FA0678) {}
+    constructor()
+        UpgradeExecutorUpgradeAction(
+            0x5613AF0474EB9c528A34701A5b1662E3C8FA0678,
+            address(new UpgradeExecutor()) // todo: deploy UpgradeExecutor separately
+        )
+    {}
 }
