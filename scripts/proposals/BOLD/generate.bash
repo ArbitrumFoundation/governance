4e60e8a7a5267d66bcfc260920c5ec26d24d79ee
Henry <11198460+godzillaba@users.noreply.github.com>
2024-12-16 20:46:42 -0500

    rollup secondary action

--- scripts/proposals/BOLD/generate.bash
+++ scripts/proposals/BOLD/generate.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # OfficeHoursAction: TODO
-# RollupUpgradeSecondaryAction: TODO
+# RollupUpgradeSecondaryAction: 0x2A3a4BDbC7c00d5115d297d83A31358B317d4740
+# - new impl at 0x5c93BAB9Ff2Fa3884b643bd8545C625De0633517
 # BoldUpgradeAction: TODO
 # SetValidatorsAction: 0x2f845d909058200e4E56855C2735975a004a4922
