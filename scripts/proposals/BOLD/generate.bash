7787a137182086995cdd664a26836cbf6cfe6be9
Henry <11198460+godzillaba@users.noreply.github.com>
2024-12-17 15:29:40 -0500

    change comment

--- scripts/proposals/BOLD/generate.bash
+++ scripts/proposals/BOLD/generate.bash
@@ -2,16 +2,14 @@
 
 # OfficeHoursAction: TODO
 
-# arb1:
-# RollupUpgradeSecondaryAction: 0x2A3a4BDbC7c00d5115d297d83A31358B317d4740
+# arb1 RollupUpgradeSecondaryAction: 0x2A3a4BDbC7c00d5115d297d83A31358B317d4740
 # - new impl at 0x5c93BAB9Ff2Fa3884b643bd8545C625De0633517
-# BoldUpgradeAction: 0xf795ec38701234664f69Dbd761Ee9c511F25ac1D
+# arb1 BoldUpgradeAction: 0xf795ec38701234664f69Dbd761Ee9c511F25ac1D
 
-# nova:
-# RollupUpgradeSecondaryAction: 0x8E1c1555b2Fe22870e7a0A454789b0c92e494ADC
+# nova RollupUpgradeSecondaryAction: 0x8E1c1555b2Fe22870e7a0A454789b0c92e494ADC
 # - new impl at 0x5c93BAB9Ff2Fa3884b643bd8545C625De0633517
-# BoldUpgradeAction: 0xd25B258B55765c9fb5567eCABB6114b03b0f78b5
-# SetValidatorsAction: 0x2f845d909058200e4E56855C2735975a004a4922
+# nova BoldUpgradeAction: 0xd25B258B55765c9fb5567eCABB6114b03b0f78b5
+# nova SetValidatorsAction: 0x2f845d909058200e4E56855C2735975a004a4922
 
 yarn gen:proposalData \
     --govChainProviderRPC https://arb1.arbitrum.io/rpc \
