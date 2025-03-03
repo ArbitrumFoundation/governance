631d3be800a51d71fcf355db476df86941730f85
gzeon <im@gzeon.dev>
2025-03-03 21:56:03 +0800

    fix: foundry pretty is now default and new format

--- test/storage/test.bash
+++ test/storage/test.bash
@@ -4,7 +4,7 @@ for CONTRACTNAME in SecurityCouncilManager L1ArbitrumTimelock ArbitrumTimelock L
 do
     echo "Checking storage change of $CONTRACTNAME"
     [ -f "$output_dir/$CONTRACTNAME" ] && mv "$output_dir/$CONTRACTNAME" "$output_dir/$CONTRACTNAME-old"
-    forge inspect "$CONTRACTNAME" --pretty storage > "$output_dir/$CONTRACTNAME"
+    forge inspect "$CONTRACTNAME" storage > "$output_dir/$CONTRACTNAME"
     diff "$output_dir/$CONTRACTNAME-old" "$output_dir/$CONTRACTNAME"
     if [[ $? != "0" ]]
     then
