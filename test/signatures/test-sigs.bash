6b1dda76203aff41bcd0330e1637e8592ea72ea7
gzeon <im@gzeon.dev>
2025-03-11 18:23:38 +0800

    fix: use new inspect script

--- test/signatures/test-sigs.bash
+++ test/signatures/test-sigs.bash
@@ -1,17 +1,3 @@
 #!/bin/bash
-output_dir="./test/signatures"
-for CONTRACTNAME in SecurityCouncilManager L1ArbitrumTimelock ArbitrumTimelock L2ArbitrumGovernor L2ArbitrumToken L1ArbitrumToken FixedDelegateErc20Wallet UpgradeExecutor SecurityCouncilMemberElectionGovernor SecurityCouncilMemberRemovalGovernor SecurityCouncilNomineeElectionGovernor
-do
-    echo "Checking for signature changes in $CONTRACTNAME"
-    [ -f "$output_dir/$CONTRACTNAME" ] && mv "$output_dir/$CONTRACTNAME" "$output_dir/$CONTRACTNAME-old"
-    forge inspect "$CONTRACTNAME" methods > "$output_dir/$CONTRACTNAME"
-    diff "$output_dir/$CONTRACTNAME-old" "$output_dir/$CONTRACTNAME"
-    if [[ $? != "0" ]]
-    then
-        CHANGED=1
-    fi
-done
-if [[ $CHANGED == 1 ]]
-then
-    exit 1
-fi
+
+./test/util/forge-inspect.bash ./test/signatures methods
