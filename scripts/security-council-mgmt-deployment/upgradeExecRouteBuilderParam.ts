c8ecc6d687272c81a289837804e1178d0bed12a6
Henry <11198460+godzillaba@users.noreply.github.com>
2025-04-23 19:55:05 -0500

    fmt

--- scripts/security-council-mgmt-deployment/upgradeExecRouteBuilderParam.ts
+++ scripts/security-council-mgmt-deployment/upgradeExecRouteBuilderParam.ts
@@ -1,27 +1,27 @@
 module.exports = [
-    [
-      {
-        chainId: 1,
-        location: {
-          inbox: "0x0000000000000000000000000000000000000000",
-          upgradeExecutor: '0x3ffFbAdAF827559da092217e474760E2b2c3CeDd'
-        }
-      },
-      {
-        chainId: 42161,
-        location: {
-          inbox: '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f',
-          upgradeExecutor: '0xCF57572261c7c2BCF21ffD220ea7d1a27D40A827'
-        }
-      },
-      {
-        chainId: 42170,
-        location: {
-          inbox: '0xc4448b71118c9071Bcb9734A0EAc55D18A153949',
-          upgradeExecutor: '0x86a02dD71363c440b21F4c0E5B2Ad01Ffe1A7482'
-        }
+  [
+    {
+      chainId: 1,
+      location: {
+        inbox: '0x0000000000000000000000000000000000000000',
+        upgradeExecutor: '0x3ffFbAdAF827559da092217e474760E2b2c3CeDd'
       }
-    ],
-    '0xE6841D92B0C345144506576eC13ECf5103aC7f49',
-    259200
+    },
+    {
+      chainId: 42161,
+      location: {
+        inbox: '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f',
+        upgradeExecutor: '0xCF57572261c7c2BCF21ffD220ea7d1a27D40A827'
+      }
+    },
+    {
+      chainId: 42170,
+      location: {
+        inbox: '0xc4448b71118c9071Bcb9734A0EAc55D18A153949',
+        upgradeExecutor: '0x86a02dD71363c440b21F4c0E5B2Ad01Ffe1A7482'
+      }
+    }
+  ],
+  '0xE6841D92B0C345144506576eC13ECf5103aC7f49',
+  259200
 ]
