ca9464776e0aa985ed4207e7ac0e38d4c8ac482c
Henry <11198460+godzillaba@users.noreply.github.com>
2025-08-13 13:10:56 -0700

    add var names to calldata generation

--- scripts/proposals/OmnibusApr25/generate.bash
+++ scripts/proposals/OmnibusApr25/generate.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Upgrade Executor Update (PROP1)
 PROP1_CHAIN_IDS=(1 42161 42170)
 PROP1_ADDRESSES=(
@@ -8,9 +10,9 @@ PROP1_ADDRESSES=(
   0xE03E930D661a729595EcC77f9e692a32BEed4260  # Arbitrum Nova
 )
 PROP1_DATAS=(
-  $(cast calldata "perform(address,address,address)" 0x5613AF0474EB9c528A34701A5b1662E3C8FA0678 0x3ffFbAdAF827559da092217e474760E2b2c3CeDd 0x3d745b8815F9be5BF053858165f8aB1F58c77932)
-  $(cast calldata "perform(address,address,address)" 0xdb216562328215E010F819B5aBe947bad4ca961e 0xCF57572261c7c2BCF21ffD220ea7d1a27D40A827 0x3d745b8815F9be5BF053858165f8aB1F58c77932)
-  $(cast calldata "perform(address,address,address)" 0xf58eA15B20983116c21b05c876cc8e6CDAe5C2b9 0x86a02dD71363c440b21F4c0E5B2Ad01Ffe1A7482 0x3d745b8815F9be5BF053858165f8aB1F58c77932)
+  $(cast calldata "perform(address admin, address target, address newLogic)" 0x5613AF0474EB9c528A34701A5b1662E3C8FA0678 0x3ffFbAdAF827559da092217e474760E2b2c3CeDd 0x3d745b8815F9be5BF053858165f8aB1F58c77932)
+  $(cast calldata "perform(address admin, address target, address newLogic)" 0xdb216562328215E010F819B5aBe947bad4ca961e 0xCF57572261c7c2BCF21ffD220ea7d1a27D40A827 0x3d745b8815F9be5BF053858165f8aB1F58c77932)
+  $(cast calldata "perform(address admin, address target, address newLogic)" 0xf58eA15B20983116c21b05c876cc8e6CDAe5C2b9 0x86a02dD71363c440b21F4c0E5B2Ad01Ffe1A7482 0x3d745b8815F9be5BF053858165f8aB1F58c77932)
 )
 PROP1_ACTION_TYPES=(0 0 0)
 PROP1_VALUES=(0 0 0)
@@ -21,7 +23,7 @@ PROP2_ADDRESSES=(
   0x8d3425f7039645223517F6F6e60Ef04C28f4188F
 )
 PROP2_DATAS=(
-  $(cast calldata "perform(address[], uint, uint, uint)" "[0xdac17f958d2ee523a2206206994597c13d831ec7]" 0 0 $(cast to-wei 0.0005))
+  $(cast calldata "perform(address[] _tokens, uint256 _maxGas, uint256 _gasPriceBid, uint256 _maxSubmissionCost)" "[0xdac17f958d2ee523a2206206994597c13d831ec7]" 0 0 $(cast to-wei 0.0005))
 )
 PROP2_ACTION_TYPES=(0)
 PROP2_VALUES=(
