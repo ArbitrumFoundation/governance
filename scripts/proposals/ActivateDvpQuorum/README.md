fde0caaa3581f5fc591a37ebb3f276361450a53d
Henry <11198460+godzillaba@users.noreply.github.com>
2026-01-28 10:45:37 -0700

    set constitution hash

--- scripts/proposals/ActivateDvpQuorum/README.md
+++ scripts/proposals/ActivateDvpQuorum/README.md
@@ -7,5 +7,5 @@ How to verify:
 
 1. Read `DeployActivateDvpQuorumUpgrade.s.sol`
 1. Run `DeployActivateDvpQuorumUpgrade.s.sol` with no rpc. Ensure the printed action address has code on arb1.
-1. Ensure that the printed action is contained in `generate.bash`
+1. Read `generate.bash` and ensure that the printed action is included
 1. Run `generate.bash` to regenerate `data.json`
--- scripts/proposals/ActivateDvpQuorum/generate.bash
+++ scripts/proposals/ActivateDvpQuorum/generate.bash
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+# todo: replace with actual constitution hash once available
+# todo: redeploy action contract with better estimate and replace address below
+
 yarn gen:proposalData \
     --govChainProviderRPC https://arb1.arbitrum.io/rpc \
+    --actionTypes \
+        0 \
+        1 \
     --actionChainIds \
+        42161 \
         42161 \
     --actionAddresses \
         0x4a3126bfAaF7B657C591988963EB32d8bd398c04 \
+        0x1D62fFeB72e4c360CcBbacf7c965153b00260417 \
+    --upgradeValues \
+        0 \
+        0 \
+    --upgradeDatas \
+        "$(cast sig "perform()")" \
+        "$(cast calldata "setConstitutionHash(bytes32)" 0x0000000000000000000000000000000000000000000000000000000000000000)" \
+    --predecessor \
+        0x0000000000000000000000000000000000000000000000000000000000000000 \
     --writeToJsonPath ./scripts/proposals/ActivateDvpQuorum/data.json
