c8f41e73a0b06f5df0ef3618a474ae900a3fb1ae
Henry <11198460+godzillaba@users.noreply.github.com>
2026-05-09 09:34:56 -0600

    30k gas and 1000gwei

--- scripts/proposals/DistributeBurnedFunds/generate.bash
+++ scripts/proposals/DistributeBurnedFunds/generate.bash
@@ -29,8 +29,8 @@ yarn gen:proposalData \
         "$(cast calldata "execute(address,address,uint256,uint256,uint256,address,uint256,bytes,address)" \
             0x554723262467F125Ac9e1cDFa9Ce15cc53822dbD \
             0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f \
-            100000 \
-            10000000000000 \
+            30000 \
+            1000000000000 \
             0 \
             $L2_RECIPIENT_ADDRESS \
             30764667401709008927568 \
